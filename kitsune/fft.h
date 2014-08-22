4a0322e251f19a1abd62ac29ea895b1f2ad9efff
Benjamin Joseph <benjo@sayhello.com>
2014-08-22 16:57:28 -0700

    unit test working for fft

--- kitsune/fft.h
+++ kitsune/fft.h
@@ -1,6 +1,10 @@
 #ifndef FFT_H
 #define FFT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int fft(short fr[], short fi[], int m);
 int fftr(short f[], int m);
 void fix_window(short fr[], int n);
@@ -12,4 +16,8 @@ unsigned long bitexp(unsigned short n);
 short fxd_sin( unsigned short x );
 unsigned int fxd_sqrt (unsigned int n);
 
+#ifdef __cplusplus
+}
+#endif
+    
 #endif
