991cc0d0c3a6205519415f2ca98c6fd444668360
Benjamin Joseph <benjo@sayhello.com>
2014-08-27 17:33:48 -0700

    Added matrix message protobuf and assorted supporting utilities. I should be able to pipe out features, debug messages, and so forth now.

---
+++ kitsune/matmessageutils.h
@@ -0,0 +1,19 @@
+
+#ifndef _MATMESSAGEUTILS_H_
+#define _MATMESSAGEUTILS_H_
+
+#include "nanopb/pb.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void SetInt16Matrix(pb_ostream_t * stream,const int16_t * data, int32_t rows, int32_t cols, uint32_t id);
+
+    
+    
+#ifdef __cplusplus
+}
+#endif
+    
+#endif
--- tools/featureextraction/pcmrunner.cpp
+++ tools/featureextraction/pcmrunner.cpp
@@ -2,18 +2,24 @@
 #include <fstream>
 #include "../../kitsune/audiofeatures.h"
 #include "../../kitsune/fft.h"
-
+#include "../../kitsune/matmessageutils.h"
+#include "../../kitsune/nanopb/pb_encode.h"
 using namespace std;
 
 #define FFT_SIZE_2N (10)
 #define FFT_SIZE (1 << FFT_SIZE_2N)
 #define SAMPLE_RATE (44100)
 
+
 int main(int argc, char * argv[]) {
     if (argc <= 2) {
         cerr << "Takes 2 inputs, 1) input file and 2) output file" << endl;
         cerr << "It is assumed that the input is mono, 16 bits per sample, in PCM format (i.e as raw as you can get)" << endl;
     }
+    
+    uint8_t buf2[16384];
+
+    pb_ostream_t output = pb_ostream_from_buffer(buf2, 16384);
     
     ifstream inFile (argv[1], ios::in | ios::binary);
     ofstream outFile(argv[2], ios::out | ios::binary);
@@ -25,15 +31,19 @@ int main(int argc, char * argv[]) {
     
     AudioFeatures_Init();
 
+    
+    //pb_encode()
+    
     do {
         //read
         inFile.read(buf,sizeof(buf));
 
         if (AudioFeatures_Extract(logmfcc,&isStable,samples, AUDIO_FFT_SIZE)) {
-            //output to file
-           // if (isStable) {
+            
+            SetInt16Matrix(&output,logmfcc,1,MEL_SCALE_ROUNDED_UP,0);
+            
                outFile.write((const char *)logmfcc,sizeof(logmfcc));
-           // }
+           
         }
         
         
