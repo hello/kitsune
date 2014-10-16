590027d72e18bae2dba5085376586992506bb75a
Jackson Chen <jchen@hello.is>
2014-10-16 16:55:48 -0700

    implementaiton of SLIP packet

--- kitsune/slip_packet.h
+++ kitsune/slip_packet.h
@@ -8,12 +8,24 @@
  */
 #ifndef __MCASP_IF_H__
 #define __MCASP_IF_H__
+
+#include "stdint.h"
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
 
+
+//resets SEQ number in slip header
+void   slip_reset(void);
+//copies user buffer to a SLIP compatible buffer
+//SEQ number is automatically generated
+//returns dest
+void * slip_write(const uint8_t * orig, uint32_t buffer_size);
+//frees buffer
+void * slip_free_buffer(void * buffer);
 
 
 
