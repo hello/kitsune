aa148a2d9bbbdcc8fe91a65027d0cbefa453b672
Jackson Chen <jchen@hello.is>
2014-10-16 16:06:00 -0700

    Added SLIP stubs

---
+++ kitsune/slip_packet.c
@@ -0,0 +1,9 @@
+/*
+ * slip_packet.c
+ *
+ *  Created on: Oct 16, 2014
+ *      Author: jacksonchen
+ */
+#include "slip_packet.h"
+
+#define SLIP_VENDOR_OPCODE 0x14
---
+++ kitsune/slip_packet.h
@@ -0,0 +1,24 @@
+/*
+ * slip_packet.c
+ *
+ *  Created on: Oct 16, 2014
+ *      Author: jacksonchen
+ *  Prepares SLIP formated UART packet for DFU
+ *
+ */
+#ifndef __MCASP_IF_H__
+#define __MCASP_IF_H__
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
