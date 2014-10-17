bd058171d68f3b9d8963387c80dff3816f4fd847
Jackson Chen <jchen@hello.is>
2014-10-17 14:59:27 -0700

    Added topboard stubs

---
+++ kitsune/top_board.h
@@ -0,0 +1,20 @@
+/**
+ * Top board control daemon
+ */
+#ifndef TOP_BOARD_H
+#define TOP_BOARD_H
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+    
+#endif
