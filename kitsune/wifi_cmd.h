31d8c2d6970df3533835c837d4685b9d92c35802
Chris Johnson <chris@sayhello.com>
2014-10-28 17:31:05 -0700

    v12

--- kitsune/wifi_cmd.h
+++ kitsune/wifi_cmd.h
@@ -103,6 +103,6 @@ void wifi_reset();
 //#define MORPH_NAME "Chris's morpheus"
 #define MORPH_NAME "test morpheus 10"
 //#define MORPH_NAME "test morpheus 80"
-#define KIT_VER 11
+#define KIT_VER 12
 
 #endif
