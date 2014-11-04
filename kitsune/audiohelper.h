b40316a2b661761d984a3e326beb090531ce2570
Benjamin Joseph <benjo@sayhello.com>
2014-11-03 17:20:30 -0800

    audio processing task can be turned off turning off this task will deallocate its storage buffer
    shell of audio playback task
    
    added audio playback to command.c
    
    Conflicts:
    	kitsune/commands.c
    
    Conflicts:
    	kitsune/commands.c
    
    audio playback task has playback code now
    
    got playback working. Yay.
    The trick was the SD CARD read speed
    conversely, make the RX buffer higher
    oh yeah, make sure the watermark is 1/2 the RX buffer size
    
    audio playback now loops until duration has expired
    
    playback disables capture and processing while playing
    this also takes care of the memory allocation issue
    
    audio playback playing nicely with commands.c now
    
    SD card clock speed back to 1Mhz
    
    rx buffer to 10k,watermark 8k,fixed global in playback
    
    audio playback buffers now off of heap
    
    reduced stack by 2K for audio playback
    
    updated commands.c to cope with changes made on master
    
    zeroed out audio playback desc
    if you don't do this, you will crash after you play a file from the
    command line
    
    audio capture task ---> audio task
    
    moved audiocapturetask --> audiotask
    
    transfering audio setup functions to audio helper
    
    audio playback in audio task now
    
    reduced memory ussage of audio processing
    fixed message priority bug in audio task

---
+++ kitsune/audiohelper.h
@@ -0,0 +1,25 @@
+#ifndef _AUDIOHELPER_H_
+#define _AUDIOHELPER_H_
+
+#include "ff.h"
+#include <stdint.h>
+
+typedef struct {
+	const char * file_name;
+	FIL file_obj;
+} Filedata_t;
+
+
+uint8_t InitAudioCapture(void);
+void DeinitAudioCapture(void);
+
+uint8_t InitAudioPlayback(int32_t vol);
+void DeinitAudioPlayback(void);
+
+
+uint8_t InitFile(Filedata_t * pfiledata);
+uint8_t WriteToFile(Filedata_t * pfiledata,const WORD bytes_to_write,const uint8_t * const_ptr_samples_bytes);
+void CloseFile(Filedata_t * pfiledata);
+
+
+#endif
