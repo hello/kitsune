91e9d68660f7c37e6c7b44f43638e0974e86ab1e
Benjamin Joseph <benjo@sayhello.com>
2014-11-03 17:20:30 -0800

    audio processing task can be turned off turning off this task will deallocate its storage buffer

--- kitsune/audioprocessingtask.h
+++ kitsune/audioprocessingtask.h
@@ -5,10 +5,19 @@
 #include "audio_types.h"
 
 
+typedef enum {
+	processingOn,
+	processingOff
+} EAudioProcessingCommand_t;
+
+
+
 
 
 void AudioProcessingTask_AddFeaturesToQueue(const AudioFeatures_t * feats);
 
+//turn it on or off via a message
+void AudioProcessingTask_SetControl(EAudioProcessingCommand_t cmd,NotificationCallback_t onFinished, void * context);
 void AudioProcessingTask_Thread(void * data);
 
 void AudioProcessingTask_FreeBuffers(void);
