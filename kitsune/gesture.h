60e90711a71adb91efec2ce5346b4059047fb96e
Jackson Chen <jchen@hello.is>
2014-11-10 10:46:41 -0800

    Added gesture stub

---
+++ kitsune/gesture.h
@@ -0,0 +1,20 @@
+/**
+ * Gesture control task
+ */
+#ifndef GESTURE_H
+#define GESTURE_H
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+    
+#endif
