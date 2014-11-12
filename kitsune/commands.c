2f6b2446808bd6c5691189d77fe68734c3686a87
Chris Johnson <chris@sayhello.com>
2014-11-12 08:47:40 -0800

    turn off the alarm on hold as well as wave

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -696,6 +696,7 @@ static void _on_wave(void * ctx){
 }
 static void _on_hold(void * ctx){
 	stop_led_animation();
+	g_ucSpkrStartFlag = 0;alarm.has_start_time = 0;
 }
 static void _on_slide(void * ctx, int delta){
 	UARTprintf("Slide delta %d\r\n", delta);
