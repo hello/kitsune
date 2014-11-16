0364be013f086c47b67638950ec1d3a700850399
Chris Johnson <plasticchris@gmail.com>
2014-11-16 03:08:30 -0800

    split up build steps

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://www.dropbox.com/s/8h4wj8hvdxw3iye/ubuntu-home-ti.tar.gz
+tar -xzf ubuntu-home-ti.tar.gz -C ~/
+
--- setup_n_build.sh
+++ setup_n_build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-wget https://www.dropbox.com/s/8h4wj8hvdxw3iye/ubuntu-home-ti.tar.gz
-tar -xzf ubuntu-home-ti.tar.gz -C ~/
-
+./setup.sh
 ./build.sh
