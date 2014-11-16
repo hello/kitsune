c7745a1aeab0a47f6c569d720e0d73951f6af1a4
Chris Johnson <chris@sayhello.com>
2014-11-16 07:47:19 -0800

    what a travisti

--- build.sh
+++ build.sh
@@ -2,12 +2,15 @@
 
 rm -rf ~/workspace
 
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./kitsune/main/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./simplelink/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./oslib/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./third_party/fatfs/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./driverlib/ccs/
 
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects  driverlib simplelink oslib fatfs
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects kitsune
+export CLASSPATH=~/hello/ti/ccsv6/ccs_base/DebugServer/packages/ti/dss/java/dss.jar:~/hello/ti/ccsv6/ccs_base/DebugServer/packages/ti/dss/java:$CLASSPATH
+
+~/hello/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./kitsune/main/ccs/
+~/hello/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./simplelink/ccs/
+~/hello/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./oslib/ccs/
+~/hello/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./third_party/fatfs/ccs/
+~/hello/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ./driverlib/ccs/
+
+~/hello/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects  driverlib simplelink oslib fatfs
+~/hello/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects kitsune
 
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://www.dropbox.com/s/8h4wj8hvdxw3iye/ubuntu-home-ti.tar.gz
-tar -xzf ubuntu-home-ti.tar.gz -C ~/
-
-export CLASSPATH=~/ti/ccsv6/ccs_base/DebugServer/packages/ti/dss/java/dss.jar:~/ti/ccsv6/ccs_base/DebugServer/packages/ti/dss/java:$CLASSPATH
-
