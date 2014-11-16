fea163694eebbb777782d83dfcb00dbe8e302c87
Chris Johnson <chris@sayhello.com>
2014-11-16 04:16:00 -0800

    add classpath

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 wget https://www.dropbox.com/s/8h4wj8hvdxw3iye/ubuntu-home-ti.tar.gz
 tar -xzf ubuntu-home-ti.tar.gz -C ~/
 
+export CLASSPATH=ti/ccsv6/ccs_base/DebugServer/packages/ti/dss/java:$CLASSPATH
+
