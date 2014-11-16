737b249ea2e9819ab90471cc8cef0664a1fb3872
Chris Johnson <plasticchris@gmail.com>
2014-11-16 02:33:01 -0800

    match path to what travis checks out

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 rm -rf ~/workspace
 
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/kitsune/main/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/simplelink/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/oslib/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/third_party/fatfs/ccs/
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/driverlib/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/hello/kitsune/kitsune/main/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/hello/kitsune/simplelink/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/hello/kitsune/oslib/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/hello/kitsune/third_party/fatfs/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/hello/kitsune/driverlib/ccs/
 
 ~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects  driverlib simplelink oslib fatfs
 ~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects kitsune
