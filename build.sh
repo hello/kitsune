7d5f9497be2c0cc0710804d956d61e009d11908c
Chris Johnson <plasticchris@gmail.com>
2014-11-15 17:50:10 -0800

    create the workspace in the build script

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace_v6_03/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects  driverlib simplelink oslib fatfs
-~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace_v6_03/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects kitsune
+rm -rf ~/workspace
+
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/kitsune/main/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/simplelink/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/oslib/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/third_party/fatfs/ccs/
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectImport -ccs.location ~/kitsune/driverlib/ccs/
+
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects  driverlib simplelink oslib fatfs
+~/ti/ccsv6/eclipse/eclipse -noSplash -data ~/workspace/ -application com.ti.ccstudio.apps.projectBuild -ccs.projects kitsune
 
