a7f6f331cfafbcc15c6bfe91d1ae31f50b84bfc1
Chris Johnson <chris@sayhello.com>
2014-11-16 21:20:51 -0800

    echo env vars

--- travis-setup.sh
+++ travis-setup.sh
@@ -2,3 +2,7 @@
 
 wget https://www.dropbox.com/s/2zr5iiuk8tezkh8/travisti.tar.gz
 tar -xzf travisti.tar.gz -C ~/
+
+echo "branch $TRAVIS_BRANCH"
+echo "tag $TRAVIS_TAG"
+echo "commit $TRAVIS_COMMIT"
