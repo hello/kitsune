f2c497782dda11405699355d2257684d944a0603
Chris Johnson <chris@sayhello.com>
2014-11-17 07:02:48 -0800

    back to BUILD_DIR

--- teardown.sh
+++ teardown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Copying build files to $TRAVIS_HOME/output"
-mkdir $TRAVIS_HOME/output
-cp $TRAVIS_BUILD_DIR/kitsune/main/ccs/exe/kitsune.bin $TRAVIS_HOME/output/
-cp $TRAVIS_BUILD_DIR/kitsune/main/ccs/Release/kitsune.map $TRAVIS_HOME/output/
+echo "Copying build files to $TRAVIS_BUILD_DIR/output"
+mkdir $TRAVIS_BUILD_DIR/output
+cp $TRAVIS_BUILD_DIR/kitsune/main/ccs/exe/kitsune.bin $TRAVIS_BUILD_DIR/output/
+cp $TRAVIS_BUILD_DIR/kitsune/main/ccs/Release/kitsune.map $TRAVIS_BUILD_DIR/output/
 
