067d2bf600f64d6d5dda3ed748dd4272ef1672ee
Chris Johnson <chris@sayhello.com>
2014-11-17 14:43:50 -0800

    fix ruby file io

--- make_set_id.rb
+++ make_set_id.rb
@@ -4,10 +4,13 @@ commit = `git show --pretty=%H 2>&1`
 version = commit.each_line().first.to_i(16) & 0xffffffff
 
 Dir.mkdir 'exe' unless File.exists? 'exe'
-File.write("./exe/build_info.txt", "version: "+version.to_s(16) + "\n")
-File.write("kitsune/kitsune_version.h", 
-  "#ifndef KIT_VER\n"+
-  "#define KIT_VER 0x" +version.to_s(16)+"\n"+
-  "#endif\n")
+File.open("./exe/build_info.txt", 'w' ) { |f|
+  f.write("version: "+version.to_s(16) + "\n")
+}
+File.open("kitsune/kitsune_version.h", 'w' ) { |f|
+  f.write ("#ifndef KIT_VER\n"+
+          "#define KIT_VER 0x" +version.to_s(16)+"\n"+
+          "#endif\n")
+}
 
 puts "Labelled as KIT_VER 0x"+version.to_s(16)+"\n"
