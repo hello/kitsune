4e4d11ec0781621b81cbf700fc105e6f6c5cbd44
Chris Johnson <chris@sayhello.com>
2014-11-17 06:20:05 -0800

    switch to absolute paths

--- teardown.sh
+++ teardown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp -r kitsune/main/ccs/exe ./
-cp kitsune/main/ccs/Release/kitsune.map ./exe
+echo "Copying build files to $TRAVIS_BUILD_DIR/build"
+mkdir $TRAVIS_BUILD_DIR/build
+cp kitsune/main/ccs/exe/kitsune.bin $TRAVIS_BUILD_DIR/build
+cp kitsune/main/ccs/Release/kitsune.map $TRAVIS_BUILD_DIR/build
 
