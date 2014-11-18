92d802ad3e2585cdfc8e3f5583be502865a6f756
Chris Johnson <chris@sayhello.com>
2014-11-17 16:52:07 -0800

    fix ruby warning

--- make_set_id.rb
+++ make_set_id.rb
@@ -4,13 +4,13 @@ commit = `git show --pretty=%H 2>&1`
 version = commit.each_line().first.to_i(16) & 0xffffffff
 
 Dir.mkdir 'exe' unless File.exists? 'exe'
-File.open("./exe/build_info.txt", 'w' ) { |f|
-  f.write("version: "+version.to_s(16) + "\n")
+File.open("./exe/build_info.txt", 'w') { |f|
+  f.write "version: "+version.to_s(16) + "\n"
 }
-File.open("kitsune/kitsune_version.h", 'w' ) { |f|
-  f.write ("#ifndef KIT_VER\n"+
+File.open("kitsune/kitsune_version.h", 'w') { |f|
+  f.write "#ifndef KIT_VER\n"+
           "#define KIT_VER 0x" +version.to_s(16)+"\n"+
-          "#endif\n")
+          "#endif\n"
 }
 
 puts "Labelled as KIT_VER 0x"+version.to_s(16)+"\n"
