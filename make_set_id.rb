0aa7da57e4c737ea4ae2e78444ae483403b906d0
Chris Johnson <chris@sayhello.com>
2014-11-17 19:10:41 -0800

    add branch and tag to build info

--- make_set_id.rb
+++ make_set_id.rb
@@ -5,7 +5,9 @@ version = commit.each_line().first.to_i(16) & 0xffffffff
 
 Dir.mkdir 'exe' unless File.exists? 'exe'
 File.open("./exe/build_info.txt", 'w') { |f|
-  f.write "version: "+version.to_s(16) + "\n"
+  f.write "version: "+version.to_s(16) + "\n" +
+          "tag: "+`git describe --tags`+
+          "branch: "+`git rev-parse --abbrev-ref HEAD`
 }
 File.open("kitsune/kitsune_version.h", 'w') { |f|
   f.write "#ifndef KIT_VER\n"+
