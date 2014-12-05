3c7db45eac12bfc65e040fae05a2130d0a6c3126
Chris Johnson <plasticchris@gmail.com>
2014-12-04 18:13:15 -0800

    reenable the debug servers for dvt

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -1415,7 +1415,7 @@ void vUARTTask(void *pvParameters) {
 	UARTprintf("*");
 	xTaskCreate(FileUploaderTask_Thread,"fileUploadTask",1*1024/4,NULL,1,NULL);
 
-#if 0 //todo DVT disable!
+#if 1 //todo PVT disable!
 	xTaskCreate(telnetServerTask,"telnetServerTask",512/4,NULL,1,NULL);
 	xTaskCreate(httpServerTask,"httpServerTask",3*512/4,NULL,1,NULL);
 #endif
