3aaa0044c2f06562d81cb85fba70e9ca9f8d624b
Benjamin Joseph <benjo@sayhello.com>
2014-12-22 17:54:06 -0800

    4k more code, smaller heap

--- kitsune/main/ccs/cc3200v1p32.cmd
+++ kitsune/main/ccs/cc3200v1p32.cmd
@@ -61,8 +61,8 @@ MEMORY
 {
     /* Application uses internal RAM for program and data */
     SRAM_DATA (RW) : origin = 0x20000000, length = 0x3FFF
-    SRAM (RWX) : origin = 0x20004000, length = 0x23FFF
-    SRAM_HEAP (RW) : origin = 0x20028000, length = 0x17FFF
+    SRAM (RWX) : origin = 0x20004000, length = 0x24FFF
+    SRAM_HEAP (RW) : origin = 0x20029000, length = 0x16FFF
 }
 
 /* Section allocation in memory */
