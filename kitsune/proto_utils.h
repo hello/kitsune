e9edadc7a0e5dfbe365eef2b96aaad6ba617521b
Pang Wu <pang@sayhello.com>
2014-12-28 15:45:31 -0800

    Add files

---
+++ kitsune/proto_utils.h
@@ -0,0 +1,31 @@
+#ifndef __PROTO_UTILS__
+#define __PROTO_UTILS__
+#include "nanopb/pb_decode.h"
+#include "nanopb/pb_encode.h"
+#include "protobuf/periodic.pb.h"
+#include "protobuf/morpheus_ble.pb.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    pill_data * pills;
+    int num_pills;
+} pilldata_to_encode;
+
+typedef struct {
+    periodic_data * data;
+    int num_data;
+} periodic_data_to_encode;
+
+
+bool encode_device_id_string(pb_ostream_t *stream, const pb_field_t *field, void * const *arg);
+bool encode_all_periodic_data (pb_ostream_t *stream, const pb_field_t *field, void * const *arg);
+bool encode_all_pills (pb_ostream_t *stream, const pb_field_t *field, void * const *arg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
