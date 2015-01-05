15f7b5aac892e704558cce4c98031546810e0b5e
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2015-01-05 14:55:23 -0800

    make different sha

--- kitsune/top_board.c
+++ kitsune/top_board.c
@@ -83,6 +83,7 @@ _on_message(uint8_t * message_body, uint32_t body_length){
 		//delay is necessary because top board is slower.
 		play_led_progress_bar(30,0,0,0, portMAX_DELAY);
 		vTaskDelay(4000);
+
 		if(0 != top_board_dfu_begin("/top/update.bin")){
 			top_board_dfu_begin("/top/factory.bin");
 		}
