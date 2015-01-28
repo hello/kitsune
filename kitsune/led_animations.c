03226c55b02b80ee3df003eb4e023975ea565a88
Chris Johnson <plasticchris@gmail.com>
2015-01-29 00:22:12 +0800

    led fade out before animation

--- kitsune/led_animations.c
+++ kitsune/led_animations.c
@@ -42,6 +42,7 @@ static void unlock() {
 static bool _start_animation( unsigned int timeout ) {
 	lock();
 	if( lock_animation( timeout ) ) {
+		led_fadeout(1);
 		LOGI("Start animation\n");
 		self.counter = 0;
 		self.sig_continue = true; //careful, to set this true requires both semaphores
