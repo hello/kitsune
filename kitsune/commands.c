1b3eafc9c3ca5621003ec23898f0ad9e8cb44626
Jackson Chen <jchen@hello.is>
2015-01-28 19:56:54 +0800

    reduce time

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -1857,7 +1857,7 @@ void Cmd_pill_test_register_heartbeat(const char * id){
 				&& 0 == strcmp(pill_fsm.uut, id)){
 			LOGF("Shake again.\r\n");
 			pill_fsm.state = WAITING_FOR_TIMEOUT;
-			pill_fsm.to = 800; //wait 8s for shake
+			pill_fsm.to = 400; //wait 8s for shake
 		}else if(pill_fsm.sem && pill_fsm.state == WAITING_FOR_END
 				&& 0 == strcmp(pill_fsm.uut, id)){
 			LOGF("Pass\r\n");
