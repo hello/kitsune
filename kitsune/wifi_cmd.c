71eb1bb54397c368bdde1de82a93ed7489689d49
Chris Johnson <plasticchris@gmail.com>
2015-02-26 14:48:04 +0800

    some debugging stuff added, commented out, remove status on boot as it causes a deadlock

--- kitsune/wifi_cmd.c
+++ kitsune/wifi_cmd.c
@@ -202,7 +202,6 @@ void SimpleLinkNetAppEventHandler(SlNetAppEvent_t *pNetAppEvent) {
 			LOGI("seeding %d\r\n", seed);
 			srand(seed); //seed with low bits of lf clock when connecting(not sure when it happens, gives some more entropy).
 		}
-		Cmd_status(0,0); //announce IP to server...
 
 		wifi_status_set(HAS_IP, false);
 
