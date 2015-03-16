b03fbc757487391f967b3b323a107d2df55e1fbb
Jackson Chen <jchen@hello.is>
2015-03-16 13:51:08 -0700

    experiment with force fade out

--- kitsune/led_animations.c
+++ kitsune/led_animations.c
@@ -307,7 +307,8 @@ void set_led_progress_bar(uint8_t percent){
 }
 
 void stop_led_animation(unsigned int delay, unsigned int fadeout){
-	ANIMATE_BLOCKING(play_led_animation_stop(fadeout),delay);
+//	ANIMATE_BLOCKING(play_led_animation_stop(fadeout),delay);
+	led_fade_out_custom_animation();
 }
 
 int Cmd_led_animate(int argc, char *argv[]){
