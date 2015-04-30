1410393a07168ef806f04b182869cc7c955d3304
Jackson Chen <jchen@hello.is>
2015-04-30 13:01:26 -0700

    pipe impl

--- kitsune/hlo_pipe.c
+++ kitsune/hlo_pipe.c
@@ -1,1 +1,66 @@
 #include "hlo_pipe.h"
+#include "task.h"
+#include "uart_logger.h"
+
+static int pipe_loop_polling(hlo_pipe_t * pipe, uint8_t * buf){
+	int read = 0;
+	int idx = 0;
+	while(read == 0){
+		read = hlo_stream_read(pipe->from, buf, pipe->buf_size);
+		if(read < 0){
+				return read;
+		}else if(read == 0){
+			vTaskDelay((TickType_t)pipe->poll_delay);
+		}
+	}
+	while(idx < read){
+		int ret = hlo_stream_write(pipe->to, buf+idx, read - idx);
+		if(ret < 0){
+			return ret;
+		}else{
+			idx += ret;
+			//TODO either poll on consumer or delay
+			vTaskDelay((TickType_t)pipe->poll_delay);
+		}
+	}
+	return 0;
+}
+
+hlo_pipe_t * hlo_pipe_new(hlo_stream_t * from, hlo_stream_t * to, uint32_t buf_size, uint32_t opt_poll_delay){
+	hlo_pipe_t * ret = pvPortMalloc(sizeof(hlo_pipe_t));
+	if(ret){
+		ret->from = from;
+		ret->to = to;
+		ret->poll_delay = opt_poll_delay;
+		ret->buf_size = buf_size;
+	}
+	return ret;
+}
+
+
+int hlo_pipe_run(hlo_pipe_t * pipe){
+	if(!pipe || !pipe->from || !pipe->to){
+		return ERROR;
+	}
+	uint8_t * buf = pvPortMalloc(pipe->buf_size);
+	if(buf){
+		int ret;
+		while((ret =  pipe_loop_polling(pipe, buf)) >= 0){
+			//do control stuff here
+		}
+		if(ret < 0){
+			DISP("Stream ended due to %d\r\n", ret);
+		}
+		vPortFree(buf);
+		return 0;
+	}
+
+	return ERROR;
+}
+
+void hlo_pipe_destroy(hlo_pipe_t * pipe){
+	//kbai
+	hlo_stream_close(pipe->from);
+	hlo_stream_close(pipe->to);
+	vPortFree(pipe);
+}
--- kitsune/hlo_pipe.h
+++ kitsune/hlo_pipe.h
@@ -7,9 +7,18 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "hlo_stream.h"
-typedef struct{
 
+typedef struct{
+	hlo_stream_t * from;
+	hlo_stream_t * to;
+	uint32_t poll_delay;
+	uint32_t buf_size;
 }hlo_pipe_t;
 
+hlo_pipe_t * hlo_pipe_new(hlo_stream_t * from, hlo_stream_t * to, uint32_t buf_size, uint32_t opt_poll_delay);
+
+int hlo_pipe_run(hlo_pipe_t * pipe);
+
+void hlo_pipe_destroy(hlo_pipe_t * pipe);
 
 #endif
