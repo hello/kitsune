4e01c393d947c0b73c98a39106946b266b95398b
Jackson Chen <jchen@hello.is>
2015-05-05 13:40:34 -0700

    added duplex codec

--- kitsune/i2c_cmd.h
+++ kitsune/i2c_cmd.h
@@ -16,6 +16,7 @@ int Cmd_readproximity(int argc, char *argv[]);
 int get_codec_NAU(int vol);
 int close_codec_NAU();
 int get_codec_mic_NAU();
+int get_codec_io_NAU(void);
 void set_volume(int v);
 
 
