a98ac8df7fd1d465f4bc554d1cfbd91a6151eca7
Jackson Chen <jchen@hello.is>
2015-05-05 15:31:23 -0700

    added octogram app

---
+++ kitsune/hlo_app_octogram.c
@@ -0,0 +1,5 @@
+#include "hlo_app_octogram.h"
+#include "hlo_audio_manager.h"
+#include "task.h"
+#include "kit_assert.h"
+
---
+++ kitsune/hlo_app_octogram.h
@@ -0,0 +1,7 @@
+#ifndef HLO_APP_OCTOGRAM_H
+#define HLO_APP_OCTOGRAM_H
+//octogram app
+//todo make it queue based
+void hlo_app_octogram_task(void * data);
+
+#endif
