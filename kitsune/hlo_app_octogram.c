756aa919a24e78275910c7bbf0555c606d735dba
Jackson Chen <jchen@hello.is>
2015-05-05 15:56:12 -0700

    Revert "added octogram app"
    This reverts commit a98ac8df7fd1d465f4bc554d1cfbd91a6151eca7.

--- kitsune/hlo_app_octogram.c
+++
@@ -1,5 +0,0 @@
-#include "hlo_app_octogram.h"
-#include "hlo_audio_manager.h"
-#include "task.h"
-#include "kit_assert.h"
-
--- kitsune/hlo_app_octogram.h
+++
@@ -1,7 +0,0 @@
-#ifndef HLO_APP_OCTOGRAM_H
-#define HLO_APP_OCTOGRAM_H
-//octogram app
-//todo make it queue based
-void hlo_app_octogram_task(void * data);
-
-#endif
