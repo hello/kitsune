f9ec300318400377247c85f87d0483f3921c44cf
Jackson Chen <jchen@hello.is>
2015-05-05 13:12:53 -0700

    audio_manager stub

---
+++ kitsune/hlo_audio_manager.c
@@ -0,0 +1,45 @@
+#include "hlo_audio_manager.h"
+#include "hlo_audio.h"
+#include "kit_assert.h"
+#include "circ_buff.h"
+
+typedef struct{
+	tCircularBuffer * buf;
+	size_t water_mark;
+}mic_client_t;
+
+static struct{
+	hlo_stream_t * master;
+	xSemaphoreHandle mic_client_lock;	//for open close and write operations
+	hlo_stream_t * mic_clients[NUM_MAX_MIC_CHANNELS];
+}self;
+
+void hlo_audio_manager_init(void){
+	self.master = hlo_audio_open_mono(48000,44,HLO_AUDIO_RECORD);
+	vSemaphoreCreateBinary(self.mic_client_lock);
+	assert(self.master);
+	assert(self.mic_client_lock);
+
+}
+static int copy_from_master(void * ctx, const void * buf, size_t size){
+	return 0;
+}
+static int copy_to_client(void * ctx, void * buf, size_t size){
+	return 0;
+}
+static int close_client(void * ctx){
+	return 0;
+}
+hlo_stream_t * hlo_open_mic_stream(size_t buffer_size, size_t opt_water_mark){
+	hlo_stream_vftbl_t tbl = {
+			.write = copy_from_master;
+			.read = copy_to_client;
+			.close = close_client;
+	};
+}
+void hlo_audio_manager_thread(void * data){
+	while(1){
+
+	}
+}
+
---
+++ kitsune/hlo_audio_manager.h
@@ -0,0 +1,29 @@
+#ifndef HLO_AUDIO_MANAGER_H
+#define HLO_AUDIO_MANAGER_H
+#include "hlo_stream.h"
+
+/**
+ * Manages multiplexes hello_audio_stream to service multiple client apps
+ */
+
+void hlo_audio_manager_init(void);
+void hlo_audio_manager_thread(void * data);
+////---------------------------------
+// Playback multiplexer
+#define NUM_MAX_PlAYBACK_CHANNELS 1
+
+int hlo_set_playback_stream(int channel, hlo_stream_t * src);
+int hlo_fade_channel(int channel);
+
+
+////---------------------------------
+// Mic multiplexer
+#define NUM_MAX_MIC_CHANNELS 3
+/**
+ * 0 < opt_water_mark <= buffer_size
+ * opt_water_mark will make the read operation return 0 if it hasn't reached
+ * leave it as 0 for no watermark.
+ */
+hlo_stream_t * hlo_open_mic_stream(size_t buffer_size, size_t opt_water_mark);
+
+#endif
