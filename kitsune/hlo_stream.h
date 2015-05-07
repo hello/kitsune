47d3f781bed2f6e3bf61cf0d9ee4cb5890f01e42
Jackson Chen <jchen@hello.is>
2015-05-06 23:28:34 -0700

    rw limit

--- kitsune/hlo_stream.h
+++ kitsune/hlo_stream.h
@@ -25,6 +25,8 @@
 typedef struct hlo_stream_info_t{
 	uint32_t bytes_written;
 	uint32_t bytes_read;
+	uint32_t write_limit;
+	uint32_t read_limit;
 	uint32_t options;
 	xSemaphoreHandle lock;
 }hlo_stream_info_t;
@@ -49,10 +51,6 @@ void hlo_stream_info(const hlo_stream_t * stream);
 int hlo_stream_write(hlo_stream_t * stream, void * buf, size_t size);
 int hlo_stream_read(hlo_stream_t * stream, void * buf, size_t size);
 
-
-//blocking pair
-//int hlo_stream_write_blocking(hlo_stream_t * stream, void * buf, size_t size);
-//int hlo_stream_read_blocking(hlo_stream_t * stream, void * buf, size_t size);
 
 int hlo_stream_close(hlo_stream_t * stream);
 
