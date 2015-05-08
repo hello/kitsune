87c4823ab7620f6d0800163e598b2aa0f6615ccc
Jackson Chen <jchen@hello.is>
2015-05-07 18:13:34 -0700

    remove pipes
    the helper functions should be sufficient

--- kitsune/hlo_pipe.c
+++ kitsune/hlo_pipe.c
@@ -2,46 +2,6 @@
 #include "task.h"
 #include "uart_logger.h"
 
-
-hlo_pipe_t * hlo_pipe_new(hlo_stream_t * from, hlo_stream_t * to, uint32_t buf_size, uint32_t opt_poll_delay){
-	hlo_pipe_t * ret = pvPortMalloc(sizeof(hlo_pipe_t));
-	if(ret){
-		ret->from = from;
-		ret->to = to;
-		ret->poll_delay = opt_poll_delay;
-		ret->buf_size = buf_size;
-	}
-	return ret;
-}
-
-
-int hlo_pipe_run(hlo_pipe_t * pipe){
-	if(!pipe || !pipe->from || !pipe->to){
-		return HLO_STREAM_NO_IMPL;
-	}
-	uint8_t * buf = pvPortMalloc(pipe->buf_size);
-	if(buf && pipe->from && pipe->to){
-		int ret;
-		while(hlo_stream_transfer_between(pipe->from, pipe->to, buf, pipe->buf_size, pipe->poll_delay) >= 0){
-			//do control stuff here
-		}
-		if(ret < 0){
-			DISP("Stream ended due to %d\r\n", ret);
-		}
-		vPortFree(buf);
-		return 0;
-	}
-
-	return HLO_STREAM_ERROR;
-}
-
-void hlo_pipe_destroy(hlo_pipe_t * pipe){
-	//kbai
-	hlo_stream_close(pipe->from);
-	hlo_stream_close(pipe->to);
-	vPortFree(pipe);
-}
-
 int hlo_stream_transfer_all(transfer_direction direction,
 							hlo_stream_t * stream,
 							uint8_t * buf,
--- kitsune/hlo_pipe.h
+++ kitsune/hlo_pipe.h
@@ -1,5 +1,5 @@
 /*
- * Pipe are a set of functions
+ * Pipes are an extension of the streams API
  * that provide blocking behavior for streams
  */
 
@@ -9,27 +9,13 @@
 #include <stdint.h>
 #include "hlo_stream.h"
 
-typedef struct{
-	hlo_stream_t * from;
-	hlo_stream_t * to;
-	uint32_t poll_delay;
-	uint32_t buf_size;
-}hlo_pipe_t;
-
-hlo_pipe_t * hlo_pipe_new(hlo_stream_t * from, hlo_stream_t * to, uint32_t buf_size, uint32_t opt_poll_delay);
-
-
-// blocks the caller thread
-int hlo_pipe_run(hlo_pipe_t * pipe);
-
-void hlo_pipe_destroy(hlo_pipe_t * pipe);
-
-////----------------------
-// helper api for a blocking transfer between a stream and a buffer.
 typedef enum{
 	INTO_STREAM = 0,
 	FROM_STREAM,
 }transfer_direction;
+
+//transfers buf_size between buffer and stream
+//polls transfer_delay ticks
 int hlo_stream_transfer_all(
 		transfer_direction direction,
 		hlo_stream_t * target,
@@ -37,7 +23,8 @@ int hlo_stream_transfer_all(
 		uint32_t buf_size,
 		uint32_t transfer_delay);
 
-//transfers buf_size data between the two pipes
+//transfers buf_size between stream and stream
+//polls transfer_delay ticks
 int hlo_stream_transfer_between(
 		hlo_stream_t * src,
 		hlo_stream_t * dst,
