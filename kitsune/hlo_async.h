632418089708c6768c1c6fe244f753585ebd8872
Jackson Chen <jchen@hello.is>
2015-05-13 18:00:20 -0700

    update comment

--- kitsune/hlo_async.h
+++ kitsune/hlo_async.h
@@ -18,26 +18,55 @@ typedef void(*future_task)(hlo_future_t * result, void * ctx);
 //standalone creation
 hlo_future_t * hlo_future_create(size_t max_size);
 
-//creates and runs the cb on the future task handler with a large stack
-//future_task is guaranteed to run sequentially
-//used to conserve space
+/**
+ * Adds the callback to @see hlo_async_task queue, calls to this api are guaranteed to be run sequentially
+ * @param max_size - size of the future objects internal buffer for write/read
+ * @param cb - the task to be scheduled
+ * @param context - context the callback
+ *
+ * @return a new object if everything checks out.
+ */
 hlo_future_t * hlo_future_create_task(size_t max_size, future_task cb, void * context);
 
-//creates a new thread to run the task
-//not guarantee for sequentiality
+/**
+ * Creates a thread to run the callback.
+ * @param max_size - size of the future object's internal buffer for write/read
+ * @param cb - the the callback will be run in a disposable thread
+ * @param context - pointer to any context to be run alongside cb
+ * @param stack_size - the stack size of the disposable thread
+ *
+ * @return a new object if everything checks out.
+ */
 hlo_future_t * hlo_future_create_task_bg(size_t max_size, future_task cb, void * context, size_t stack_size);
 
 
-//producer
+/**
+ *  @param future - the future you wish to write to
+ *  @param buffer -  copies the content of this buffer into the future's internal buffer
+ *  @param size - size of your buffer
+ *  @param return_code  - return code to be returned by hlo_future_read
+ *  @return expect 0, <0 if theres a buffer error.
+ */
 int hlo_future_write(hlo_future_t * future, const void * buffer, size_t size, int return_code);
 
-//consumer use this
+/**
+ *  @param future - the future you wish to read from
+ *  @param buf -  copies the content of the future's internal buffer into this
+ *  @param size - size of your buffer
+ *  @param ms - delay to wait
+ *  @return the return code set by hlo_future_write.  Check with implementer, but typically < 0 are errros
+ */
 int hlo_future_read(hlo_future_t * future,  void * buf, size_t size, TickType_t ms);
 
-//destroy after read
+/**
+ * destroys a future
+ * use after a read, otherwise it'll crash the system.
+ */
 void hlo_future_destroy(hlo_future_t * future);
 
-//rtos scheduler
+/**
+ * run this to support synchronous mode aka @see hlo_future_create_task
+ */
 void hlo_async_task(void * ctx);
 
 
--- kitsune/hlo_net_tools.h
+++ kitsune/hlo_net_tools.h
@@ -10,7 +10,10 @@ unsigned long resolve_ip_by_host_name(const char * host_name);
 
 //scans and copies a list of unique result
 int get_unique_wifi_list(Sl_WlanNetworkEntry_t * result, size_t num_entries);
+
 //gives you a future result.
+//IMPORTANT, a read response will return the number of actual entries
+//and not the number of bytes written
 hlo_future_t * prescan_wifi(size_t num_entries);
 
 int Cmd_dig(int argc, char *argv[]);
