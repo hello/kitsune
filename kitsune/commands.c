1073c7afb17222bad5500d12b9a916c1135391eb
Jackson Chen <jchen@hello.is>
2015-06-18 14:01:18 -0700

    rate limit

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -891,6 +891,15 @@ static char * str_prox(void){
 	}
 	return str;
 }
+static bool needs_sample(portTickType * tag){
+	bool ret = false;
+	portTickType now = xTaskGetTickCount();
+	if( (now - *tag) > (10 * 1000) ){
+		ret = true;
+		*tag = now;
+	}
+	return ret;
+}
 void thread_fast_i2c_poll(void * unused)  {
 	unsigned int filter_buf[3];
 	unsigned int filter_idx=0;
@@ -901,7 +910,8 @@ void thread_fast_i2c_poll(void * unused)  {
 	prox_fifo = pvPortMalloc(20 * 5 * sizeof(int));
 	memset(prox_fifo, 0, 20 * 5 * sizeof(int));
 	uint32_t counter = 0;
-
+	portTickType wave_tag, hold_tag, release_tag;
+	wave_tag = hold_tag = release_tag = xTaskGetTickCount();
 	while (1) {
 		portTickType now = xTaskGetTickCount();
 		int prox=0;
@@ -927,16 +937,22 @@ void thread_fast_i2c_poll(void * unused)  {
 			case proxGestureWave:
 				_on_wave();
 				gesture_increment_wave_count();
-				analytics_event( "{wave_data: %s}", str_prox());
+				if(needs_sample(&wave_tag)){
+					analytics_event( "{wave_data: %s}", str_prox());
+				}
 				break;
 			case proxGestureHold:
 				_on_hold();
 				gesture_increment_hold_count();
-				analytics_event( "{hold_data: %s}", str_prox());
+				if(needs_sample(&hold_tag)){
+					analytics_event( "{hold_data: %s}", str_prox());
+				}
 				break;
 			case proxGestureRelease:
 				_on_gesture_out();
-				analytics_event( "{release_data: %s}", str_prox());
+				if(needs_sample(&release_tag)){
+					analytics_event( "{release_data: %s}", str_prox());
+				}
 				break;
 			default:
 				break;
