dfa18ebab3f2df116d329b7a35b64a0a1cd50233
Benjamin Joseph <benjo@sayhello.com>
2015-06-18 13:59:52 -0700

    added simple hold test

--- unit-test/testprox.cpp
+++ unit-test/testprox.cpp
@@ -20,7 +20,7 @@ protected:
 };
 
 
-TEST_F(TestProx,TestFlatSignal) {
+TEST_F(TestProx,TestSimpleWave) {
     ProxSignal_Init();
     ProxGesture_t gesture = proxGestureNone;
     
@@ -33,6 +33,30 @@ TEST_F(TestProx,TestFlatSignal) {
     gesture = ProxSignal_UpdateChangeSignals(18000);
     
     ASSERT_TRUE(gesture == proxGestureWave);
+    
+    
+}
+
+
+TEST_F(TestProx,TestSimpleHold) {
+    ProxSignal_Init();
+    ProxGesture_t gesture = proxGestureNone;
+    
+    for (int t = 0; t < 100; t++) {
+        ProxSignal_UpdateChangeSignals(20000);
+    }
+    
+    bool foundHold = false;
+    
+    for (int t = 0; t < 100; t++) {
+        gesture = ProxSignal_UpdateChangeSignals(9000);
+        std::cout << gesture << std::endl;
+        if (gesture == proxGestureHold) {
+            foundHold = true;
+        }
+    }
+    
+    ASSERT_TRUE(foundHold);
     
     
 }
