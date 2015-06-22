07d28e3adb4ce02a79853c952fee82a3aa5f1be3
Chris Johnson <plasticchris@gmail.com>
2015-06-23 00:42:50 +0800

    remove unused function prototype

--- kitsune/i2c_cmd.h
+++ kitsune/i2c_cmd.h
@@ -6,8 +6,6 @@ int Cmd_i2c_write(int argc, char *argv[]);
 int Cmd_i2c_readreg(int argc, char *argv[]);
 int Cmd_i2c_writereg(int argc, char *argv[]);
 
-void Cmd_james();
-
 int Cmd_readtemp(int argc, char *argv[]);
 int Cmd_readhumid(int argc, char *argv[]);
 int Cmd_readlight(int argc, char *argv[]);
