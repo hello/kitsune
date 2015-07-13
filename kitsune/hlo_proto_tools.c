49d2c7559941c86dade63da3f68a74d03cdd8008
Chris Johnson <plasticchris@gmail.com>
2015-07-14 07:01:53 +0800

    more stack for the async worker

--- kitsune/hlo_proto_tools.c
+++ kitsune/hlo_proto_tools.c
@@ -86,6 +86,6 @@ hlo_future_t * MorpheusCommand_from_buffer(void * buf, size_t size){
 
 }
 hlo_future_t * buffer_from_MorpheusCommand(MorpheusCommand * src){
-	return hlo_future_create_task_bg(encode_MorpheusCommand, src, 1024);
+	return hlo_future_create_task_bg(encode_MorpheusCommand, src, 1536);
 }
 
