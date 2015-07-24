7575fa74d56e5a5ad4d4a14f662c0bb1b0feb83c
Jackson Chen <jchen@hello.is>
2015-07-24 16:06:08 -0700

    Add prototype for iterating directory

--- kitsune/fs_utils.h
+++ kitsune/fs_utils.h
@@ -1,6 +1,10 @@
 #ifndef __FS_UTILS_H__
 #define __FS_UTILS_H__
+#include "hellofilesystem.h"
+
+typedef void (*file_itr) (const char * file_name, const FILINFO * info);
 
 int fs_get( char * file, void * data, int max_rd, int * len );
 int fs_save( char* file, void* data, int len);
+int fs_list( char * dir, file_itr itr);
 #endif
