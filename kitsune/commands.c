005cf2be9db71ac6b739a6666bbe4028127d4110
Jackson Chen <jchen@hello.is>
2015-11-18 12:37:27 -0800

    skip pill data upload

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -1001,7 +1001,6 @@ int data_queue_batch_size = 1;
 int pill_queue_batch_size = PILL_BATCH_WATERMARK;
 
 void thread_tx(void* unused) {
-	batched_pill_data pill_data_batched = {0};
 	batched_periodic_data data_batched = {0};
 	periodic_data forced_data;
 	bool got_forced_data = false;
@@ -1093,28 +1092,11 @@ void thread_tx(void* unused) {
 		}
 
 		if (uxQueueMessagesWaiting(pill_queue) > pill_queue_batch_size) {
-			LOGI(	"sending  pill data\n" );
-			pilldata_to_encode pilldata;
-			pilldata.num_pills = 0;
-			pilldata.pills = (pill_data*)pvPortMalloc(MAX_BATCH_PILL_DATA*sizeof(pill_data));
-
-			if( !pilldata.pills ) {
-				LOGI( "failed to alloc pilldata\n" );
-				vTaskDelay(1000);
-				continue;
+			//dump them
+			pill_data dummy = {0};
+			while( xQueueReceive(pill_queue, &dummy, 1 ) ) {
+				DISP("dumping pill data\\r\n");
 			}
-
-			while( pilldata.num_pills < MAX_BATCH_PILL_DATA && xQueueReceive(pill_queue, &pilldata.pills[pilldata.num_pills], 1 ) ) {
-				++pilldata.num_pills;
-			}
-
-			memset(&pill_data_batched, 0, sizeof(pill_data_batched));
-			pill_data_batched.pills.funcs.encode = encode_all_pills;  // This is smart :D
-			pill_data_batched.pills.arg = &pilldata;
-			pill_data_batched.device_id.funcs.encode = encode_device_id_string;
-
-			send_pill_data(&pill_data_batched);
-			vPortFree( pilldata.pills );
 		}
 		do {
 			if( xQueueReceive(force_data_queue, &forced_data, 2000 ) ) {
