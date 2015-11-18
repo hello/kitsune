2eed7eb0900daea8f241dbc9e7f3566ad4fe4e6b
Jackson Chen <jchen@hello.is>
2015-11-17 18:59:06 -0800

    limit logger 0

--- kitsune/uart_logger.h
+++ kitsune/uart_logger.h
@@ -29,7 +29,7 @@ extern "C" {
 #define UART_LOGGER_MODE	UART_LOGGER_MODE_TAGGED
 
 //how many files maximum to keep
-#define UART_LOGGER_FILE_LIMIT 512
+#define UART_LOGGER_FILE_LIMIT 0
 
 /**
  * Tag level defines
