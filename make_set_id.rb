10e3e804cb25f930459f8a09a1c4767c00cfe63a
Chris <chris@sayhello.com]>
2016-01-13 16:17:31 -0800

    update travis script

--- make_set_id.rb
+++ make_set_id.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
 commit = `git show --pretty=%H 2>&1`
-version = commit.each_line().first.to_i(16) & 0x7fffffff
+version = commit.each_line().first.to_s()[0,7]
 
 Dir.mkdir 'exe' unless File.exists? 'exe'
 File.open("./exe/build_info.txt", 'w') { |f|
