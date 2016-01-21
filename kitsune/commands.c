56b167737058fa976e1dd05f5849b275f80890ed
Chris <chris@sayhello.com]>
2016-01-21 10:53:12 -0800

    Reduce system load during audio
    demote led driver task priority
    remove spin waits, replace with semaphores
    clear wdt on idle

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -987,7 +987,7 @@ void thread_fast_i2c_poll(void * unused)  {
 	}
 }
 
-#define MAX_PERIODIC_DATA 90
+#define MAX_PERIODIC_DATA 30
 #define MAX_PILL_DATA 20
 #define MAX_BATCH_PILL_DATA 10
 #define PILL_BATCH_WATERMARK 0
@@ -2031,7 +2031,7 @@ void vUARTTask(void *pvParameters) {
 	if(led_init() != 0){
 		LOGI("Failed to create the led_events.\n");
 	}
-	xTaskCreate(led_task, "ledTask", 700 / 4, NULL, 1, NULL);
+	xTaskCreate(led_task, "ledTask", 700 / 4, NULL, 2, NULL);
 	xTaskCreate(led_idle_task, "led_idle_task", 256 / 4, NULL, 4, NULL);
 
 	//switch the uart lines to gpios, drive tx low and see if rx goes low as well
