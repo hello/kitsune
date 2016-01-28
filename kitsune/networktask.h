82c7dad6d13f20cee24c42aab299e048f37dccf2
Chris <chris@sayhello.com]>
2016-01-28 15:51:52 -0800

    single socket for the network task

--- kitsune/networktask.h
+++ kitsune/networktask.h
@@ -61,7 +61,7 @@ void networktask_init(uint16_t stack_size);
  * field in the HTTP headers, the socket it will talk on will
  * always connect to DATA_SERVER
  */
-bool NetworkTask_SendProtobuf(bool blocking, const char * host,
+bool NetworkTask_SendProtobuf(bool blocking, char * host,
 		const char * endpoint, const pb_field_t fields[],
 		void * structdata, int32_t retry_time_in_counts,
 		NetworkResponseCallback_t func, void * context,
