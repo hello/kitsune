0aca830343eb8b4e176874034740babf4072245c
Chris <chris@sayhello.com]>
2016-02-01 10:33:11 -0800

    long poll debugging

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -1766,7 +1766,7 @@ void launch_tasks() {
 	UARTprintf("*");
 	xTaskCreate(thread_tx, "txTask", 1536 / 4, NULL, 4, NULL);
 	UARTprintf("*");
-	//long_poll_task_init( 4096 / 4 );
+	long_poll_task_init( 4096 / 4 );
 #endif
 }
 
