f6b92b70ef9988ce10b85a21ed9998e5dd631458
Divya Kamath <divkam1987@gmail.com>
2016-02-19 14:24:59 -0800

    Fixes for path and filename

--- kitsune/fatfs_cmd.c
+++ kitsune/fatfs_cmd.c
@@ -1469,8 +1469,9 @@ void file_download_task( void * params ) {
 
 				// SHA verify for SD card files
 				char buf[64];
+				strcpy(buf, "/");
 				strncpy(buf, path, 64 );
-				strncat(buf, "/", 1 );
+				strcat(buf, "/");
 				strncat(buf, filename, 64 );
 
 				if (download_info.has_sha1) {
