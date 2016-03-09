c2b951b1a056209b9bf171b0dc2bbb98f4f8249e
Divya Kamath <divkam1987@gmail.com>
2016-03-09 13:19:30 -0800

    Added queues, mutex etc.

--- kitsune/main/ccs/filedownloadmanager.c
+++ kitsune/main/ccs/filedownloadmanager.c
@@ -1,15 +1,25 @@
 #include "filedownloadmanager.h"
 #include "wifi_cmd.h"
 #include "file_manifest.pb.h"
+#include "FreeRTOS.h"
 #include "queue.h"
 #include "semphr.h"
 
+#define FILE_ERROR_QUEUE_DEPTH (10)
+
+// Response received semaphore
+static xSemaphoreHandle _response_received_sem = NULL;
+
 // File error queue
+static xQueueHandle _file_error_queue = NULL;
 
 // file download status flag
+static bool file_download_status = false; // true if download is in progress, false otherwise.
 // mutex to protect file download status
+static xSemaphoreHandle _file_download_mutex = NULL;
 
 // link health - send and recv errors
+static FileManifest_LinkHealth link_health = {0};
 
 // Static Functions
 static void DownloadManagerTask(void * filesyncdata);
@@ -21,15 +31,26 @@ static void _on_file_sync_response_failure( );
 // Init
 void downloadmanagertask_init(uint16_t stack_size)
 {
+	// init error queue
+	if (!_file_error_queue) {
+		_file_error_queue = xQueueCreate(FILE_ERROR_QUEUE_DEPTH,sizeof(FileManifest_FileOperationError));
+	}
+
+	// init mutex for file download status flag
+	if(!_file_download_mutex)
+	{
+		_file_download_mutex = xSemaphoreCreateMutex();
+	}
+
+	// init sempahore that indicates server response has been received
+	if (!_response_received_sem) {
+		_response_received_sem = xSemaphoreCreateBinary();
+	}
+
+
 	//this task needs a larger stack because
 	//some protobuf encoding will happen on the stack of this task
 	xTaskCreate(DownloadManagerTask, "downloadManagerTask", stack_size, NULL, 2, NULL);
-
-	// init error queue
-
-	// init query delay
-
-	// init link health error count
 
 
 }
@@ -37,6 +58,19 @@ void downloadmanagertask_init(uint16_t stack_size)
 // Download Manager task
 static void DownloadManagerTask(void * filesyncdata)
 {
+
+	// init query delay
+
+	// init link health error count
+	link_health.send_errors = 0;
+	link_health.time_to_response = 0;
+
+	// init file download status flag
+	xSemaphoreTake(_file_download_mutex, portMAX_DELAY);
+		file_download_status = false;
+	xSemaphoreGive(_file_download_mutex);
+
+
 	protobuf_reply_callbacks pb_cb;
 
     pb_cb.get_reply_pb = _get_file_sync_response;
@@ -49,7 +83,7 @@ static void DownloadManagerTask(void * filesyncdata)
 
 	for (; ;)
 	{
-		//if(xSemaphoreTake() == pdTrue)
+		//if(xSemaphoreTake(_response_received_sem,portMAX_DELAY) == pdTrue)
 		{
 			// save time to response count
 
