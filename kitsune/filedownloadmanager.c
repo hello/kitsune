c879630baa624ee67b70489854f7a964c2dfe1ca
Divya Kamath <divkam1987@gmail.com>
2016-03-15 18:00:39 -0700

    Partial testing and some more changes
    - Updated pb files
    - Testing: 200 OK received from server for partial file manifest upload
    - Some incomplete updates to file scan

--- kitsune/filedownloadmanager.c
+++ kitsune/filedownloadmanager.c
@@ -15,14 +15,30 @@
 
 
 #define FILE_ERROR_QUEUE_DEPTH (10)		// ensure that this matches the max_count for error_info in file_manifest.options
-#define QUERY_DELAY_DEFAULT		(15UL) //minutes
+
+// TODO REMEMBER TO CHANGE THIS *******
+#define QUERY_DELAY_DEFAULT		(1UL) //minutes
+
+
 #define PATH_BUF_MAX_SIZE		(64)
 #define FOLDERS_TO_EXCLUDE      (2)
+
+// TODO can this define be a part of the protobuf
+#define MAX_NUMBER_OF_FILES		(50)	// Maximum number of files that will be uploaded in the file manifest
 
 const char* folders[FOLDERS_TO_EXCLUDE] = {"LOGS", "USR"};
 
 typedef struct {
-	FileManifest_FileDownload * data;
+	//File path
+	char path[48];
+
+	//Filename
+	char filename[12];
+}file_list_t;
+
+typedef struct {
+	//FileManifest_FileDownload* data;
+	file_list_t* ga_file_list;//[MAX_NUMBER_OF_FILES];
     uint32_t num_data;
 } file_info_to_encode;
 
@@ -46,6 +62,8 @@ static FileManifest_LinkHealth link_health = {0};
 
 static uint32_t message_sent_time;
 
+//static file_list_t ga_file_list[MAX_NUMBER_OF_FILES];
+
 // Query delay in ticks
 TickType_t query_delay_ticks = (QUERY_DELAY_DEFAULT*60*1000)/portTICK_PERIOD_MS;
 
@@ -56,7 +74,7 @@ static void _free_file_sync_response(void * structdata);
 static void _on_file_sync_response_success( void * structdata);
 static void _on_file_sync_response_failure(void );
 static void get_file_download_status(FileManifest_FileStatusType* file_status);
-static bool _on_file_update(pb_istream_t *stream, const pb_field_t *field, void **arg);
+bool _on_file_update(pb_istream_t *stream, const pb_field_t *field, void **arg);
 static void free_file_sync_info(FileManifest_FileDownload * download_info);
 static void restart_download_manager(void);
 bool encode_file_info (pb_ostream_t *stream, const pb_field_t *field, void * const *arg);
@@ -90,6 +108,8 @@ void downloadmanagertask_init(uint16_t stack_size)
 
 	//stack_size? //TODO
 	xTaskCreate(DownloadManagerTask, "downloadManagerTask", stack_size, NULL, 2, NULL);
+
+	file_manifest_local.ga_file_list = NULL;
 
 
 }
@@ -150,8 +170,12 @@ static void DownloadManagerTask(void * filesyncdata)
 			LOGI("DM: Sem Taken\n");
 			vTaskDelayUntil(&start_time,query_delay_ticks); // TODO should this be vtaskdelay instead?
 
+			memset(&message_for_upload,0,sizeof(FileManifest));
 			/* UPDATE FILE INFO */
 
+			/*
+			file_manifest_local.num_data = 0;
+			//file_manifest_local.data = ((FileManifest_FileDownload*)pvPortMalloc(MAX_NUMBER_OF_FILES*sizeof(FileManifest_FileDownload)));
 			// Scan through file system and update file manifest
 			uint32_t ret = update_file_manifest();
 			if(ret)
@@ -161,9 +185,10 @@ static void DownloadManagerTask(void * filesyncdata)
 			}
 
 			LOGI("File manifest created for uploading \n");
+			*/
 
-			message_for_upload.file_info.funcs.encode = encode_file_info;
-			message_for_upload.file_info.arg = &file_manifest_local;
+			//message_for_upload.file_info.funcs.encode = encode_file_info;
+			//message_for_upload.file_info.arg = &file_manifest_local;
 
 			/* UPDATE FILE STATUS - DOWNLOADED/PENDING */
 
@@ -200,9 +225,10 @@ static void DownloadManagerTask(void * filesyncdata)
 			message_for_upload.has_sd_card_size = true;
 			//TODO update memory info
 			message_for_upload.sd_card_size.has_free_memory = true;
-			message_for_upload.sd_card_size.free_memory = get_free_space();
+			message_for_upload.sd_card_size.free_memory = get_free_space(); //TODO
+			LOGI("DM: Free space: %d \n", message_for_upload.sd_card_size.free_memory);
 			message_for_upload.sd_card_size.has_total_memory = true;
-			//message_for_upload.sd_card_size.total_memory = 0; //TODO
+			message_for_upload.sd_card_size.total_memory = 0; //TODO
 
 			/* UPDATE FILE ERROR INFO */
 
@@ -227,11 +253,13 @@ static void DownloadManagerTask(void * filesyncdata)
 
 			message_sent_time = xTaskGetTickCount();
 
+			//30 sec
 			//TODO are all parameters right
 			if(NetworkTask_SendProtobuf(
 					true, DATA_SERVER, FILE_SYNC_ENDPOINT, FileManifest_fields, &message_for_upload, 0, NULL, NULL, &pb_cb, false)
 					!= 0 )
 			{
+				// TODO why do I get this even with a 200 from server
 				LOGI("File manifest failed to upload \n");
 
 				/*
@@ -303,10 +331,10 @@ bool _on_file_update(pb_istream_t *stream, const pb_field_t *field, void **arg)
 
 
 	// decode PB
-	LOGI("file sync parsing\n" );
+	LOGI("DM: file sync parsing\n" );
 	if( !pb_decode(stream,FileManifest_File_fields,&file_info) )
 	{
-		LOGI("file sync - parse fail \n" );
+		LOGI("DM: file sync - parse fail \n" );
 		free_file_sync_info( &file_info.download_info );
 		return false;
 	}
@@ -373,7 +401,26 @@ void free_file_sync_info(FileManifest_FileDownload * download_info)
 //TODO
 bool encode_file_info (pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
 {
+	/*
+    int i;
+    periodic_data_to_encode * data = *(periodic_data_to_encode**)arg;
 
+    for( i = 0; i < data->num_data; ++i ) {
+        if(!pb_encode_tag(stream, PB_WT_STRING, batched_periodic_data_data_tag))
+        {
+            LOGI("encode_all_periodic_data: Fail to encode tag error %s\n", PB_GET_ERROR(stream));
+            return false;
+        }
+
+        if (!pb_encode_delimited(stream, periodic_data_fields, &data->data[i])){
+            LOGI("encode_all_periodic_data2: Fail to encode error: %s\n", PB_GET_ERROR(stream));
+            return false;
+        }
+        //LOGI("******************* encode_pill_encode_all_pills: encode pill %s\n", pill_data.deviceId);
+    }
+    */
+
+    // free file_manifest to encode
 	return true;
 
 }
@@ -420,14 +467,17 @@ static void _on_file_sync_response_success( void * structdata)
 	LOGF("_on_file_sync_response_success\r\n");
 
 	link_health.time_to_response = (xTaskGetTickCount() - message_sent_time) / configTICK_RATE_HZ /60; // in minutes
+	LOGI("DM: Time to response %d \n", link_health.time_to_response );
 
 	// If has update file and delete file info
 		// send for download
+
 
 	// Update query delay ticks
 	if(response_protobuf->has_query_delay)
 	{
 		query_delay_ticks = (response_protobuf->query_delay * 60 * 1000)/portTICK_PERIOD_MS;
+		LOGI("DM: Query delay %d\n", response_protobuf->query_delay);
 	}
 	else
 	{
@@ -456,6 +506,10 @@ static void _on_file_sync_response_failure( )
     //TODO is there any way to know if error occurred during send or recv
 	link_health.send_errors++;
 
+	/*NOTE: This semaphore give helps to retry sending file manifest even when there is a failure.
+	 * Since retries are handled here, the retry time in count for NetworkTask_sendProtobuf() is set to 0.
+	 * If that were to change to >0, then the semapahore below needs to be removed
+	 */
     // give semaphore for default query delay
 	restart_download_manager();
 
@@ -476,13 +530,38 @@ static inline void restart_download_manager(void)
 	xSemaphoreGive(_response_received_sem);
 }
 
+static uint32_t running_file_count;
+
 static uint32_t update_file_manifest(void)
 {
 	uint32_t res;
 	char path_buf[PATH_BUF_MAX_SIZE] = {0};
+	uint32_t i;
 
 	strncpy(path_buf,"/",sizeof(path_buf));
+
+	running_file_count = 0;
+	file_manifest_local.num_data = 0;
+	file_manifest_local.ga_file_list = NULL;
 	res = scan_files(path_buf);
+	file_manifest_local.num_data = running_file_count;
+
+	LOGI("DM: File count: %d\n", file_manifest_local.num_data);
+
+	// Loop
+	for(i=0;i<file_manifest_local.num_data;i++)
+	{
+		/*
+	    // TODO Update file info
+	    file_manifest_local.data[i].sd_card_filename.funcs.encode = _encode_string_fields;
+
+	    file_manifest_local.data[i].sd_card_filename.arg = ga_file_list[i].filename;
+
+	    file_manifest_local.data[i].sd_card_path.funcs.encode = _encode_string_fields;
+	    file_manifest_local.data[i].sd_card_path.arg = ga_file_list[i].path;
+	    */
+	}
+
 
 	return res;
 
@@ -530,6 +609,25 @@ static uint32_t scan_files(char* path)
                 if (res != FR_OK) break;
             } else {                                       /* It is a file. */
                 LOGI("DM full path: %s/%s\n", path, fn);
+
+                /*
+				if(running_file_count >= file_manifest_local.num_data ||
+						!file_manifest_local.ga_file_list)
+				{
+					file_manifest_local.num_data = ( file_manifest_local.num_data ) ?
+							file_manifest_local.num_data*2 : 2;
+
+					file_manifest_local.ga_file_list =
+							pvPortRealloc(file_manifest_local.ga_file_list, file_manifest_local.num_data * sizeof(FileManifest_FileDownload));
+				}
+
+				// assert pointer TODO
+                strncpy(file_manifest_local.ga_file_list[running_file_count].path,path,48);
+                strncpy(file_manifest_local.ga_file_list[running_file_count].filename,fn,12);
+
+                running_file_count++;
+                */
+
             }
             vTaskDelay(50/portTICK_PERIOD_MS);
         }
