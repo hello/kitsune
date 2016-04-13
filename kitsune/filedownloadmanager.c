9e5a97db6f3cc9628497c0f69526ff4bd1f008b8
Divya Kamath <divkam1987@gmail.com>
2016-04-13 14:51:26 -0700

    Updated buffer to not be static
    - changed since it can be called from multiple tasks simultaneously

--- kitsune/filedownloadmanager.c
+++ kitsune/filedownloadmanager.c
@@ -519,6 +519,8 @@ static bool scan_files(char* path, pb_ostream_t *stream, const pb_field_t *field
 
 	            		total_file_count++;
 
+	            		//LOGI("DM: sha_ovwr :%d\n", sha_ovwr);
+
 	            		if(update_sha_file(path,fn, sha_file_create,NULL, sha_ovwr ))
 						{
 							LOGE("DM: Error creating SHA\n");
@@ -717,7 +719,7 @@ static int32_t sd_sha_verifynsave(const char * sha_truth, char* path, char* sha_
 #define minval( a,b ) a < b ? a : b
 
 	uint8_t sha[SHA1_SIZE] = { 0 };
-    static uint8_t buffer[SD_BLOCK_SIZE];
+    uint8_t* buffer;
     uint32_t bytes_to_read, bytes_read;
     uint32_t bytes_to_write = 0, bytes_written = 0;
     FIL fp = {0};
@@ -726,6 +728,9 @@ static int32_t sd_sha_verifynsave(const char * sha_truth, char* path, char* sha_
 
     SHA1_CTX sha1ctx;
     SHA1_Init(&sha1ctx);
+
+    buffer = (uint8_t*) pvPortMalloc(SD_BLOCK_SIZE);
+    assert(buffer);
 
     //fetch path info
     //LOGI( "computing SHA of %s\n", path);
@@ -747,7 +752,7 @@ static int32_t sd_sha_verifynsave(const char * sha_truth, char* path, char* sha_
     while (bytes_to_read > 0) {
 		vTaskDelay(5/portTICK_PERIOD_MS);
 
-		res = hello_fs_read(&fp, buffer,(minval(sizeof(buffer),bytes_to_read)), &bytes_read);
+		res = hello_fs_read(&fp, buffer,(minval(SD_BLOCK_SIZE,bytes_to_read)), &bytes_read);
 		if (res) {
 			LOGE("DM: f_read %d\n", res);
 			return -1;
@@ -803,6 +808,9 @@ static int32_t sd_sha_verifynsave(const char * sha_truth, char* path, char* sha_
 
 	// Close file
 	hello_fs_close(&fp);
+
+	if(buffer)
+		vPortFree(buffer);
 
 
     return 0;
@@ -871,8 +879,6 @@ uint32_t update_sha_file(char* path, char* original_filename, update_sha_t optio
 	if(get_complete_filename(sha_fullpath,sha_filename, path, PATH_BUF_MAX_SIZE))
 		return ~0;
 
-
-
 	// Check if SHA file exists
 	file_exists = (does_sha_file_exist(sha_fullpath)) ? true: false;
 
@@ -883,10 +889,15 @@ uint32_t update_sha_file(char* path, char* original_filename, update_sha_t optio
 			// debug log for why function was called
 			//LOGI("DM: SHA CREATE \n");
 
-			if(file_exists && !ovwr )
+			// Check if downloads are going on
+			// FileManifest_FileStatusType download_pending;
+			// get_file_download_status(&download_pending);
+
+			if((file_exists && !ovwr))
 			{
 				// File exists and overwrite flag is false,
 				// no need to update
+				//LOGI("DM: download_pending:%d\n", (uint8_t)download_pending);
 				return 0;
 			}
 			LOGI("DM: SHA create %s\n", sha_filename);
