9f0420c25ed97f4cff17420b9017e77229f77dd1
Chris <chris@sayhello.com]>
2016-04-19 15:58:08 -0700

    increase download task stack

--- kitsune/commands.c
+++ kitsune/commands.c
@@ -2205,7 +2205,7 @@ void vUARTTask(void *pvParameters) {
 	load_data_server();
 
 	xTaskCreate(AudioTask_Thread,"audioTask",2560/4,NULL,4,NULL);
-	init_download_task( 2560 / 4 );
+	init_download_task( 3072 / 4 );
 	networktask_init(3 * 1024 / 4);
 
 	load_serial();
