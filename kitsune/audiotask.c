8d0c01e87ea82c647c80ddbb66368847f280ee63
Jackson Chen <jchen@hello.is>
2016-05-04 14:35:18 -0700

    removed audio manager

--- kitsune/audiotask.c
+++ kitsune/audiotask.c
@@ -31,6 +31,7 @@
 #include "hlo_async.h"
 #include "state.pb.h"
 #include "hlo_audio_tools.h"
+#include "hlo_audio_manager.h"
 #if 0
 #define PRINT_TIMING
 #endif
@@ -224,6 +225,86 @@ static uint8_t CheckForInterruptionDuringPlayback(void) {
 extern xSemaphoreHandle i2c_smphr;
 
 bool add_to_file_error_queue(char* filename, int32_t err_code, bool write_error);
+typedef struct{
+	unsigned long current;
+	unsigned long target;
+	unsigned long ramp_up_ms;
+	unsigned long ramp_down_ms;
+}ramp_ctx_t;
+extern xSemaphoreHandle i2c_smphr;
+extern bool set_volume(int v, unsigned int dly);
+
+static void _change_volume_task(hlo_future_t * result, void * ctx){
+	volatile ramp_ctx_t * v = (ramp_ctx_t*)ctx;
+	while( v->target || v->current ){
+		if(v->current > v->target){
+			vTaskDelay(v->ramp_down_ms);
+			v->current--;
+		}else if(v->current < v->target){
+			v->current++;
+			vTaskDelay(v->ramp_up_ms);
+		}else{
+			vTaskDelay(10);
+			continue;
+		}
+		//fallthrough if volume adjust is needed
+		LOGI("Setting volume %u at %u\n", v->current, xTaskGetTickCount());
+		if( xSemaphoreTakeRecursive(i2c_smphr, 100)) {
+			//set vol
+			vTaskDelay(5);
+			set_volume(v->current, 0);
+			vTaskDelay(5);
+			xSemaphoreGiveRecursive(i2c_smphr);
+
+		}else{
+			//set volume failed, instantly exit out of this async worker.
+			hlo_future_write(result, NULL, 0, -1);
+		}
+
+	}
+	hlo_future_write(result, NULL, 0, 0);
+}
+////-------------------------------------------
+//playback sample app
+static void _playback_loop(AudioPlaybackDesc_t * desc, audio_control_signal sig_stop){
+	int ret;
+	uint8_t chunk[512]; //arbitrary
+
+	if(!desc || !desc->stream || !sig_stop){
+		return;
+	}
+	portTickType end = xTaskGetTickCount() + desc->durationInSeconds * 1000;
+	ramp_ctx_t vol;
+	vol.current = 0;
+	vol.target = desc->volume;
+	vol.ramp_up_ms = desc->fade_in_ms / (desc->volume + 1);
+	vol.ramp_down_ms = desc->fade_out_ms / (desc->volume + 1);
+
+	hlo_stream_t * spkr = hlo_open_spkr_stream( 0 );
+	hlo_stream_t * fs = desc->stream;
+
+	hlo_future_t * vol_task = (hlo_future_t*)hlo_future_create_task_bg(_change_volume_task,(void*)&vol,1024);
+
+	while( (ret = hlo_stream_transfer_between(fs,spkr,chunk, sizeof(chunk),2)) > 0){
+		if( sig_stop() || xTaskGetTickCount() >= end){
+			//signal ramp down
+			vol.target = 0;
+		}
+		if(hlo_future_read(vol_task,NULL,0,0) >= 0){
+			//future task has completed
+			break;
+		}
+	}
+	vol.target = 0;
+	hlo_future_destroy(vol_task);
+	hlo_stream_close(fs);
+	hlo_stream_close(spkr);
+	if(desc->onFinished){
+		desc->onFinished(desc->context);
+	}
+	DISP("Playback Task Finished %d\r\n", ret);
+}
+
 static uint8_t DoPlayback(const AudioPlaybackDesc_t * info) {
 	uint8_t returnFlags = 0x00;
 
@@ -236,20 +317,12 @@ static uint8_t DoPlayback(const AudioPlaybackDesc_t * info) {
 		return returnFlags;
 	}
 
-
-/*	if ( !InitAudioPlayback(initial_volume, info->rate ) ) {
-		LOGE("unable to initialize audio playback!\r\n");
-		vPortFree(speaker_data);
-		DeinitAudioPlayback();
-		_queue_audio_playback_state(SILENT, info);
-		return returnFlags;
-	}*/
 	LOGI("%d free %d stk\n", xPortGetFreeHeapSize(),  uxTaskGetStackHighWaterMark(NULL));
 
 	_queue_audio_playback_state(PLAYING, info);
 
 	/* blocking */
-	hlo_audio_playback_task(info, CheckForInterruptionDuringPlayback);
+	_playback_loop(info, CheckForInterruptionDuringPlayback);
 
 
 	LOGI("completed playback\r\n");
