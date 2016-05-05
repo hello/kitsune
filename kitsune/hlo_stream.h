9efd7267a0fdf55256d1a42f8f5509db878876a7
JChen <zer0page@users.noreply.github.com>
2016-05-04 22:17:42 -0700

    Update hlo_stream.h

--- kitsune/hlo_stream.h
+++ kitsune/hlo_stream.h
@@ -47,7 +47,7 @@ typedef struct{
 //view info
 void hlo_stream_info(const hlo_stream_t * stream);
 
-//nonblocking pair
+//nonblocking and threadsafe (incl. implementer's method as well.)
 int hlo_stream_write(hlo_stream_t * stream, const void * buf, size_t size);
 int hlo_stream_read(hlo_stream_t * stream, void * buf, size_t size);
 
