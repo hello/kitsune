8edb25254528ea95c3476079a726642ae9ce3899
Jackson Chen <jchen@hello.is>
2016-05-11 11:29:41 -0700

    better documentation

--- kitsune/hlo_stream.h
+++ kitsune/hlo_stream.h
@@ -10,9 +10,9 @@
 #include "semphr.h"
 
 //options
-#define HLO_STREAM_WRITE		1
-#define HLO_STREAM_READ	 		2
-#define HLO_STREAM_READ_WRITE	(HLO_STREAM_WRITE + HLO_STREAM_READ)
+#define HLO_STREAM_WRITE		(1 << 0)
+#define HLO_STREAM_READ	 		(1 << 1)
+#define HLO_STREAM_READ_WRITE	(HLO_STREAM_WRITE | HLO_STREAM_READ)
 
 //steam return codes
 #define HLO_STREAM_ERROR		-1
@@ -20,8 +20,6 @@
 #define HLO_STREAM_EAGAIN		-11
 #define HLO_STREAM_NULL_OBJ		-20
 #define HLO_STREAM_NO_IMPL		-21
-
-
 
 //meta info for the parent api
 typedef struct hlo_stream_info_t{
@@ -46,12 +44,19 @@ typedef struct{
 //view info
 void hlo_stream_info(const hlo_stream_t * stream);
 
-//nonblocking and threadsafe (incl. implementer's method as well.)
+/**
+ * Threadsafe pair of API to operate on streams.
+ * The expected behavior is "best effort blocking", meaning
+ * it'll block for a sensible amount of time while consuming/reading as much as the passed @size
+ * as possible.
+ *
+ *  Use the hlo_stream_transfer_* API in hlo_pipe.h for a guaranteed full transfer of the buffer.
+ */
 int hlo_stream_write(hlo_stream_t * stream, const void * buf, size_t size);
 int hlo_stream_read(hlo_stream_t * stream, void * buf, size_t size);
 
 /**
- * closes the stream, the stream object is considered invalid after this call
+ * Threadsafe, closes the stream, the stream object is considered invalid after this call
  * IMPORTANT: Any of the following condition, when met, does not deallocate the wrapper object's memory 
  * 1.  the close method is not implemented (Usually means it's a singleton stream that lives on the static memory)
  * 2.  the close method is implemented, but returns an error (Usually means it has deinit errors)
@@ -70,6 +75,9 @@ void hlo_stream_init(hlo_stream_t * stream,
 //do not use in conjunction with init.
 hlo_stream_t * hlo_stream_new(const hlo_stream_vftbl_t * impl, void * ctx, uint32_t options);
 
+/**
+ * example implementations
+ */
 //fifo buffer stream
 hlo_stream_t * fifo_stream_open(size_t capacity);
 //random stream
