35a49c6a9e0fdd2d490b2b46833d725bfc7b5050
Jackson Chen <jchen@hello.is>
2016-05-13 18:45:00 -0700

    remove unused code

--- kitsune/hlo_net_tools.h
+++ kitsune/hlo_net_tools.h
@@ -3,10 +3,6 @@
 #include "hlo_async.h"
 #include "wlan.h"
 
-/**
- * host name must remain in scope
- */
-unsigned long resolve_ip_by_host_name(const char * host_name);
 
 //scans and copies a list of unique result
 int get_unique_wifi_list(Sl_WlanNetworkEntry_t * result, size_t num_entries);
@@ -17,9 +13,6 @@ int get_unique_wifi_list(Sl_WlanNetworkEntry_t * result, size_t num_entries);
 hlo_future_t * prescan_wifi(size_t num_entries);
 
 
-hlo_future_t * http_get_stream(const char  * url);
-
-int Cmd_dig(int argc, char *argv[]);
 int Cmd_scan_wifi(int argc, char *argv[]);
-int Cmd_test_get(int argc, char * argv[]);
+
 #endif
