6230937f1907215994cb5ed18c77548a5fe6c2e3
Jackson Chen <jchen@hello.is>
2016-05-18 12:45:15 -0700

    close is ok on positive value

--- kitsune/hlo_stream.c
+++ kitsune/hlo_stream.c
@@ -52,7 +52,7 @@ int hlo_stream_close(hlo_stream_t * stream){
 	LOCK(stream);
 	ret = stream->impl.close(stream->ctx);
 	UNLOCK(stream);
-	if(ret == 0){
+	if(ret >= 0){
 		vSemaphoreDelete(stream->info.lock);
 		vPortFree(stream);
 	}else if( ret == HLO_STREAM_NO_IMPL ){
