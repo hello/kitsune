47e985c909e692a575fd8d4aa9233e788a8930da
Jackson Chen <jchen@hello.is>
2016-06-02 13:03:44 -0700

    remove signand verify stream

--- kitsune/hlo_proto_tools.h
+++ kitsune/hlo_proto_tools.h
@@ -9,18 +9,4 @@ hlo_future_t * MorpheusCommand_from_buffer(void * buf, size_t size);
 hlo_future_t * buffer_from_MorpheusCommand(MorpheusCommand * src);
 
 #include "hlo_stream.h"
-/**
- * wrapper class for signing a stream
- * base stream is managed by this
- * signature is appended onto the data
- * WRITE ONLY
- */
-hlo_stream_t * sign_stream(const hlo_stream_t * base);
-/**
- * wrapper class for verifying a stream
- * base stream is managed by this
- * signature is prepended onto the data
- * READ_ONLY
- */
-hlo_stream_t * verify_stream(const hlo_stream_t * base);
 #endif
