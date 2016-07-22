977f82c3916258c417e634631c2538cf60dc4071
Chris <chris@sayhello.com]>
2016-07-22 11:08:16 -0700

    use smaller fft, 7sec

--- kitsune/tensor/keyword_net.c
+++ kitsune/tensor/keyword_net.c
@@ -1,5 +1,5 @@
 #include "keyword_net.h"
-#include "model_may25_lstm_large.c"
+#include "model_may25_lstm_small_okay_sense_tiny.c"
 #include "tinytensor_features.h"
 #include "tinytensor_memory.h"
 #include "uartstdio.h"
