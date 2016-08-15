328a71220027e5de46ceefd04d91dd0d12c808fc
Divya Kamath <divkam1987@gmail.com>
2016-08-15 11:51:04 -0700

    undo playback loop change

--- kitsune/audiotask.c
+++ kitsune/audiotask.c
@@ -224,11 +224,8 @@ static void _playback_loop(AudioPlaybackDesc_t * desc, hlo_stream_signal sig_sto
 	hlo_future_t * vol_task = (hlo_future_t*)hlo_future_create_task_bg(_change_volume_task,(void*)&vol,1024);
 
 	//playback
-#if 0 //TODO DKH
 	hlo_filter transfer_function = desc->p ? desc->p : hlo_filter_data_transfer;
-#else
-	hlo_filter transfer_function = hlo_filter_data_transfer;
-#endif
+
 	ret = transfer_function(fs, spkr, desc->context, sig_stop);
 
 	//join async worker
