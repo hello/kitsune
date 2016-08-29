00165125713f441f735a352691d61359af638f3f
Chris <chris@sayhello.com]>
2016-08-29 12:58:14 -0700

    more reasonable isr wait

--- kitsune/hlo_audio.c
+++ kitsune/hlo_audio.c
@@ -71,7 +71,7 @@ static int _write_playback_mono(void * ctx, const void * buf, size_t size){
 		if(!is_playback_active()){
 			set_isr_playback(true);
 		}
-		if(!xSemaphoreTake(playback_isr_sem,10)){
+		if(!xSemaphoreTake(playback_isr_sem,100)){
 			LOGI("ISR Failed\r\n");
 #if 1
 			Audio_Stop();
@@ -113,7 +113,7 @@ static int _open_record(){
 }
 static int _read_record_mono(void * ctx, void * buf, size_t size){
 	if( !IsBufferSizeFilled(pTxBuffer, LISTEN_WATERMARK) ){
-		if(!xSemaphoreTake(record_isr_sem,10)){
+		if(!xSemaphoreTake(record_isr_sem,100)){
 			LOGI("ISR Failed\r\n");
 #if 1
 			Audio_Stop();
