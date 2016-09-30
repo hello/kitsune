4d48738e023c5df46834e36b08c937fbd8850b08
Chris <chris@sayhello.com]>
2016-09-30 13:40:26 -0700

    fix the tone, remove output filters, divya found the bug!

--- kitsune/pcm_handler.c
+++ kitsune/pcm_handler.c
@@ -307,10 +307,10 @@ void DMAPingPongCompleteAppCB_opt()
 				for (i = CB_TRANSFER_SZ/4-1; i!=-1 ; --i) {
 					//the odd ones do not matter
 					/*ping[(i<<1)+1] = */
-					ping_p[(i<<2) + 0] = (ping_p[i] & 0xFFFFUL) << 16;
 					ping_p[(i<<2) + 1] = 0;
 					ping_p[(i<<2) + 2] = (ping_p[i] & 0xFFFF0000);
 					ping_p[(i<<2) + 3] = 0;
+					ping_p[(i<<2) + 0] = (ping_p[i] & 0xFFFFUL) << 16;
 				}
 #else
 				for (i = CB_TRANSFER_SZ/2-1; i!=-1 ; --i) {
@@ -343,10 +343,10 @@ void DMAPingPongCompleteAppCB_opt()
 				for (i = CB_TRANSFER_SZ/4-1; i!=-1 ; --i) {
 					//the odd ones do not matter
 					/*ping[(i<<1)+1] = */
-					pong_p[(i<<2) + 0] = (pong_p[i] & 0xFFFFUL) << 16;
 					pong_p[(i<<2) + 1] = 0;
 					pong_p[(i<<2) + 2] = (pong_p[i] & 0xFFFF0000);
 					pong_p[(i<<2) + 3] = 0;
+					pong_p[(i<<2) + 0] = (pong_p[i] & 0xFFFFUL) << 16;
 				}
 #else
 					for (i = CB_TRANSFER_SZ/2-1; i!=-1 ; --i) {
