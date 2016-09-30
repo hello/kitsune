45a0e00f54001b7e614312e85e1942e9506b8250
Chris <chris@sayhello.com]>
2016-09-30 15:51:50 -0700

    wake word buffer

--- kitsune/codec/hlo_lossless.c
+++ kitsune/codec/hlo_lossless.c
@@ -15,13 +15,48 @@ static const char magic_number[5] = "hello";
 static const uint32_t frame_sync = 0xAA55FF00;
 static const int samples_per_channel = BLOCK_SIZE;
 
+
+hlo_stream_t * hlo_lossless_init_chunkbuf( int size ) {
+	return circ_stream_open(size);
+}
+int hlo_lossless_write_chunkbuf( hlo_stream_t * s, int16_t short_samples[] ) {
+	return hlo_lossless_write_frame(s, short_samples);
+}
+int hlo_lossless_dump_chunkbuf( hlo_stream_t * s, hlo_stream_t * output ) {
+	uint32_t word;
+	int ret = 1;
+	uint8_t buf[512];
+
+	//scan for frame sync
+	while( ret ) {
+		ret = hlo_stream_transfer_all(FROM_STREAM, s, (uint8_t*)&word,sizeof(int32_t), 4);
+		if (ret < 0 ) break;
+		//since we are guaranteed writes work on the circ stream there much be a whole number
+		//of frames after the first sync. It's possible frames of different sizes come in, in which case
+		//an incoming frame could eat the header off the eldest frame to make room
+		if( word == frame_sync ) {
+			hlo_lossless_start_stream(output);
+			ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&frame_sync,sizeof(int32_t), 4);
+			if (ret < 0 ) break;
+			ret = hlo_stream_transfer_between(s,output,buf,sizeof(buf),4);
+			if (ret < 0 ) break;
+		}
+	}
+	hlo_stream_close(s);
+	return ret;
+}
+
 int hlo_lossless_start_stream( hlo_stream_t * output) {
 	int ret = 1;
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&magic_number,sizeof(magic_number), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&sample_rate,sizeof(int32_t), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&bps,sizeof(int16_t), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&channels,sizeof(int8_t), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&estimated_frames,sizeof(int32_t), 4);
+	ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&magic_number,sizeof(magic_number), 4);
+	if (ret < 0 ) return ret;
+	ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&sample_rate,sizeof(int32_t), 4);
+	if (ret < 0 ) return ret;
+	ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&bps,sizeof(int16_t), 4);
+	if (ret < 0 ) return ret;
+	ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&channels,sizeof(int8_t), 4);
+	if (ret < 0 ) return ret;
+	ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&estimated_frames,sizeof(int32_t), 4);
 	return ret;
 }
 int hlo_lossless_write_frame(hlo_stream_t * output, int16_t short_samples[] ) {
@@ -48,7 +83,8 @@ int hlo_lossless_write_frame(hlo_stream_t * output, int16_t short_samples[] ) {
 	uint16_t req_int_ref,req_int_residues;
 	uint32_t req_bits_ref,req_bits_residues;
 
-	ret &= hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&frame_sync,sizeof(int32_t), 4);
+	ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&frame_sync,sizeof(int32_t), 4);
+	if (ret < 0 ) return ret;
 	for(i = 0; i < channels; i++)
 	{
 		//Separate channels
@@ -112,19 +148,23 @@ int hlo_lossless_write_frame(hlo_stream_t * output, int16_t short_samples[] ) {
 		req_int_residues = (req_bits_residues+31)/(32);
 
 		//Write rice_params,bytes,encoded lpc_coeffs to output
-		ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&rice_param_ref,sizeof(uint8_t), 4);
-		ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&req_int_ref,sizeof(uint16_t), 4);
-		ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&opt_lpc_order,sizeof(uint8_t), 4);
-		ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)encoded_ref,req_int_ref*sizeof(uint32_t), 4);
+		ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&rice_param_ref,sizeof(uint8_t), 4);
+		if (ret < 0 ) break;
+		ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&req_int_ref,sizeof(uint16_t), 4);
+		if (ret < 0 ) break;
+		ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&opt_lpc_order,sizeof(uint8_t), 4);
+		if (ret < 0 ) break;
+		ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)encoded_ref,req_int_ref*sizeof(uint32_t), 4);
+		if (ret < 0 ) break;
 
 		//Write rice_params,bytes,encoded residues to output
-		ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&rice_param_residue,sizeof(uint8_t), 4);
-		ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&req_int_residues,sizeof(uint16_t), 4);
-		ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&encoded_residues,req_int_residues*sizeof(uint32_t), 4);
+		ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&rice_param_residue,sizeof(uint8_t), 4);
+		if (ret < 0 ) break;
+		ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&req_int_residues,sizeof(uint16_t), 4);
+		if (ret < 0 ) break;
+		ret = hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&encoded_residues,req_int_residues*sizeof(uint32_t), 4);
+		if (ret < 0 ) break;
 
-		if (ret < 0 ){
-			break;
-		}
 	}
 	return ret;
 }
@@ -133,12 +173,12 @@ int hlo_filter_lossless_encoder(hlo_stream_t * input, hlo_stream_t * output, voi
 	int16_t short_samples[BLOCK_SIZE];
 	int ret = hlo_lossless_start_stream(output);
 
-	while(ret >= 0){
+	while(1){
 		ret = hlo_stream_transfer_all(FROM_STREAM, input, (uint8_t*)short_samples, sizeof(short_samples) /*read_size*bps/8*/, 4);
-		if ( ret < 0 ){
-			break;
-		}
+		if (ret < 0 ) break;
+
 		ret = hlo_lossless_write_frame( output, short_samples /*, (ret/(bps/8))/channels*/ );
+		if (ret < 0 ) break;
 
 		BREAK_ON_SIG(signal);
 	}
--- kitsune/codec/hlo_lossless.h
+++ kitsune/codec/hlo_lossless.h
@@ -5,4 +5,7 @@ int hlo_filter_lossless_encoder(hlo_stream_t * input, hlo_stream_t * output, voi
 int hlo_lossless_start_stream( hlo_stream_t * output);
 int hlo_lossless_write_frame(hlo_stream_t * output, int16_t short_samples[] );
 
+hlo_stream_t * hlo_lossless_init_chunkbuf( int size );
+int hlo_lossless_write_chunkbuf( hlo_stream_t * s, int16_t short_samples[] );
+int hlo_lossless_dump_chunkbuf( hlo_stream_t * s, hlo_stream_t * output );
 #endif
