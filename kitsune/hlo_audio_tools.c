d9be7e4c329614b0638bfdedb72fd9adfb644d39
Chris <chris@sayhello.com]>
2016-09-30 10:34:28 -0700

    lossless codec in voice control

--- kitsune/hlo_audio_tools.c
+++ kitsune/hlo_audio_tools.c
@@ -26,6 +26,8 @@
 AudioState get_audio_state();
 
 #include "speech.pb.h"
+
+#include "codec/hlo_lossless.h"
 ////-------------------------------------------
 //The feature/extractor processor we used in sense 1.0
 //
@@ -126,145 +128,6 @@ int hlo_filter_adpcm_encoder(hlo_stream_t * input, hlo_stream_t * output, void *
 				break;
 			}
 		}
-		BREAK_ON_SIG(signal);
-	}
-	return ret;
-}
-////------------------------------------------
-#include "codec/lpc.h"
-#include "codec/rice.h"
-#define BLOCK_SIZE 128
-
-int hlo_filter_lossless_encoder(hlo_stream_t * input, hlo_stream_t * output, void * ctx, hlo_stream_signal signal){
-	const int32_t sample_rate = 16000;
-	const int16_t bps = 16;
-	const int8_t channels = 1;
-	const int32_t estimated_frames = 800; //6 seconds
-	uint8_t opt_lpc_order = 0;
-	uint8_t rice_param_ref,rice_param_residue;
-	const char magic_number[5] = "hello";
-	uint16_t req_int_ref,req_int_residues,samples_per_channel;
-	const int16_t Q = 35;
-	int32_t i,j = 0;
-	const uint32_t frame_sync = 0xAA55FF00;
-	uint32_t req_bits_ref,req_bits_residues;
-	const int64_t corr = ((int64_t)1) << Q;
-
-	//Arrays
-	int16_t short_samples[BLOCK_SIZE];
-	int32_t qtz_ref_coeffs[MAX_LPC_ORDER];
-	int32_t int_samples[BLOCK_SIZE];
-	int32_t residues[BLOCK_SIZE];
-	uint32_t unsigned_ref[MAX_LPC_ORDER];
-	uint32_t encoded_ref[MAX_LPC_ORDER];
-	uint32_t u_residues[BLOCK_SIZE];
-	uint32_t encoded_residues[BLOCK_SIZE];
-	int64_t lpc[MAX_LPC_ORDER + 1];
-	int32_t qtz_samples[BLOCK_SIZE];
-	int32_t autocorr[MAX_LPC_ORDER + 1];
-	int32_t ref[MAX_LPC_ORDER];
-	int32_t lpc_mat[MAX_LPC_ORDER][MAX_LPC_ORDER];
-
-	int ret = 1;
-
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&magic_number,sizeof(magic_number), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&sample_rate,sizeof(int32_t), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&bps,sizeof(int16_t), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&channels,sizeof(int8_t), 4);
-	ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&estimated_frames,sizeof(int32_t), 4);
-
-	//Define read size
-	int32_t read_size = channels * BLOCK_SIZE;
-	int16_t * buffer = short_samples;
-
-	while(ret >= 0){
-		ret = hlo_stream_transfer_all(FROM_STREAM, input, (uint8_t*)short_samples, read_size*bps/8, 4);
-		if ( ret < 0 ){
-			break;
-		}
-		ret /= bps/8;
-
-		samples_per_channel = ret/channels;
-
-		ret &= hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&frame_sync,sizeof(int32_t), 4);
-		for(i = 0; i < channels; i++)
-		{
-			//Separate channels
-			for(j = 0; j < samples_per_channel; j++)
-				short_samples[j] = buffer[channels * j + i];
-			//Quantize sample data
-			for(j = 0; j < samples_per_channel; j++)
-				qtz_samples[j] = (short_samples[j]<<15);
-			//Calculate autocorrelation data
-			auto_corr_fun(qtz_samples,samples_per_channel,MAX_LPC_ORDER,1,
-				autocorr);
-			//Calculate reflection coefficients
-			opt_lpc_order = compute_ref_coefs(autocorr,MAX_LPC_ORDER,ref);
-			//Quantize reflection coefficients
-			qtz_ref_cof(ref,opt_lpc_order,qtz_ref_coeffs);
-			//signed to unsigned
-			signed_to_unsigned(opt_lpc_order,qtz_ref_coeffs,unsigned_ref);
-
-			//get optimum rice param and number of bits
-			rice_param_ref = get_opt_rice_param(unsigned_ref,opt_lpc_order,
-				&req_bits_ref);
-			//Encode ref coeffs
-			req_bits_ref = rice_encode_block(rice_param_ref,unsigned_ref,
-				opt_lpc_order,encoded_ref);
-			//Determine number of ints required for storage
-			req_int_ref = (req_bits_ref+31)/(32);
-
-			//Dequantize reflection
-			dqtz_ref_cof(qtz_ref_coeffs,opt_lpc_order,ref);
-
-			//Reflection to lpc
-			levinson(NULL,opt_lpc_order,ref,lpc_mat);
-
-			lpc[0] = 0;
-			for(j = 0; j < opt_lpc_order; j++)
-				lpc[j + 1] = corr * lpc_mat[opt_lpc_order - 1][j];
-
-			for(j = opt_lpc_order; j < MAX_LPC_ORDER; j++)
-				lpc[j + 1] = 0;
-
-			//Copy samples
-			for(j = 0; j < samples_per_channel; j++)
-				int_samples[j] = short_samples[j];
-
-			//Get residues
-			calc_residue(int_samples,samples_per_channel,opt_lpc_order,Q,lpc,
-				residues);
-
-			//signed to unsigned
-			signed_to_unsigned(samples_per_channel,residues,u_residues);
-
-			//get optimum rice param and number of bits
-			rice_param_residue = get_opt_rice_param(u_residues,
-				samples_per_channel,&req_bits_residues);
-
-			//Encode residues
-			req_bits_residues = rice_encode_block(rice_param_residue,u_residues,
-				samples_per_channel,encoded_residues);
-
-			//Determine nunber of ints required for storage
-			req_int_residues = (req_bits_residues+31)/(32);
-
-			//Write rice_params,bytes,encoded lpc_coeffs to output
-			ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&rice_param_ref,sizeof(uint8_t), 4);
-			ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&req_int_ref,sizeof(uint16_t), 4);
-			ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&opt_lpc_order,sizeof(uint8_t), 4);
-			ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)encoded_ref,req_int_ref*sizeof(uint32_t), 4);
-
-			//Write rice_params,bytes,encoded residues to output
-			ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&rice_param_residue,sizeof(uint8_t), 4);
-			ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&req_int_residues,sizeof(uint16_t), 4);
-			ret &= 0<hlo_stream_transfer_all(INTO_STREAM, output, (uint8_t*)&encoded_residues,req_int_residues*sizeof(uint32_t), 4);
-
-			if (ret < 0 ){
-				break;
-			}
-		}
-
 		BREAK_ON_SIG(signal);
 	}
 	return ret;
@@ -438,8 +301,6 @@ int hlo_filter_voice_command(hlo_stream_t * input, hlo_stream_t * output, void *
 	int16_t samples[NSAMPLES];
 	uint8_t hmac[SHA1_SIZE] = {0};
 
-	char compressed[NUM_SAMPLES_TO_RUN_FFT/2];
-
 	adpcm_state state = (adpcm_state){0};
 	bool light_open = false;
 
@@ -483,14 +344,15 @@ int hlo_filter_voice_command(hlo_stream_t * input, hlo_stream_t * output, void *
 				send_str = hlo_stream_bw_limited( send_str, AUDIO_NET_RATE/8, 5000);
 				light_open = true;
 
-				hlo_pb_encode(send_str, speech_data_fields, &nn_ctx.speech_pb);
+				//hlo_pb_encode(send_str, speech_data_fields, &nn_ctx.speech_pb);
 				speech_detected_time = xTaskGetTickCount();
+
+				ret = hlo_lossless_start_stream(send_str);
 			} else {
-				adpcm_coder((short*)samples, (char*)compressed, ret / 2, &state);
-				ret = hlo_stream_transfer_all(INTO_STREAM, send_str,  (uint8_t*)compressed, sizeof(compressed), 4);
-				if( ret < 0 ) {
-					break;
-				}
+				ret = hlo_lossless_write_frame(send_str, samples);
+			}
+			if( ret < 0 ) {
+				break;
 			}
 
 			if (!nn_ctx.is_speaking) {
