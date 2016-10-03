95903d7b5ef861cf37e0de3b8fdeae7e0a19be28
Jackson Chen <jchen@hello.is>
2016-10-03 15:34:53 -0700

    update constants to account for change in sample siz

--- kitsune/tensor/tinytensor_features.c
+++ kitsune/tensor/tinytensor_features.c
@@ -42,7 +42,7 @@ void set_background_energy(const int16_t fr[], const int16_t fi[]);
 #define START_SPEECH_THRESHOLD (4000)
 #define STOP_SPEECH_THRESHOLD (1000)
 
-#define NUM_NONSPEECH_FRAMES_TO_TURN_OFF   (50)
+#define NUM_NONSPEECH_FRAMES_TO_TURN_OFF   (33)
 #define NUM_SPEECH_FRAMES_TO_TURN_ON       (2)
 //hanning window
 __attribute__((section(".data")))
@@ -147,7 +147,7 @@ void tinytensor_features_get_mel_bank(int16_t * melbank,const int16_t * fr, cons
 
 void tinytensor_features_force_voice_activity_detection(void) {
 	_this.is_speech = 1;
-	_this.num_nonspeech_frames = -150;
+	_this.num_nonspeech_frames = -100;
 }
 
 #define SPEECH_BIN_START (4)
