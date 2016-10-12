612970de71498b43e3bc934dc62906b1424e1a23
Divya Kamath <divkam1987@gmail.com>
2016-10-12 12:40:32 -0700

    fade smoothly if audio upload err

--- kitsune/hlo_audio_tools.c
+++ kitsune/hlo_audio_tools.c
@@ -384,7 +384,7 @@ int hlo_filter_voice_command(hlo_stream_t * input, hlo_stream_t * output, void *
 
 	if (ret < 0) {
 		if( ret != HLO_STREAM_EAGAIN ) {
-			stop_led_animation(0, 33);
+			stop_led_animation(2, 33);
 		}
 		if (ret == HLO_STREAM_ERROR) {
 			play_led_animation_solid(LED_MAX, LED_MAX, 0, 0, 1, 18, 1);
