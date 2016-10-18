25f6d0ea68f10be52c75bb1ca3c7fcd8836ec855
Benjamin Joseph <benjo@sayhello.com>
2016-10-17 17:26:26 -0700

    save point

---
+++ kitsune/audio_features_upload_task.h
@@ -0,0 +1,8 @@
+#ifndef _AUDIO_FEATURES_UPLOAD_TASK_H_
+#define _AUDIO_FEATURES_UPLOAD_TASK_H_
+
+void audio_features_upload_task_add_message(const AudioFeaturesUploadTaskMessage_t * message);
+
+
+
+#endif //_AUDIO_FEATURES_UPLOAD_TASK_H_
