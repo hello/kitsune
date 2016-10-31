a057edba8d0f9eb0dd1d8da072a0d004f5e8f8b2
Benjamin Joseph <benjo@sayhello.com>
2016-10-31 10:48:02 -0700

    rolled back neural net to previous to this one: model_aug30_lstm_med_dist_okay_sense_stop_snooze_tiny_fa8_1014_ep105

--- kitsune/tensor/keyword_net.c
+++ kitsune/tensor/keyword_net.c
@@ -13,7 +13,7 @@
 #include "arm_const_structs.h"
 #include "fft.h"
 
-#define NEURAL_NET_MODEL "model_aug30_lstm_med_dist_okay_sense_stop_snooze_tiny_end0_1022_ep052.c" 
+#define NEURAL_NET_MODEL "model_aug30_lstm_med_dist_okay_sense_stop_snooze_tiny_fa8_1014_ep105.c"
 #include NEURAL_NET_MODEL
 const static char * k_net_id = NEURAL_NET_MODEL;
 
