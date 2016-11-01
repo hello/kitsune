464f14eb21ae75e6bd86b9a3f9bc8d1241782209
Benjamin Joseph <benjo@sayhello.com>
2016-11-01 16:55:15 -0700

    some prints needed line endings

--- kitsune/audio_features_upload_task.c
+++ kitsune/audio_features_upload_task.c
@@ -167,12 +167,12 @@ void audio_features_upload_trigger_async_upload(const char * net_id,const char *
 	_current_delay = DELAY_TIME; //reset the delay time
 
 	if (_is_waiting_for_uploading) {
-		LOGI("audio_features_upload -- upload already in the pipe, ignoring upload request");
+		LOGI("audio_features_upload -- upload already in the pipe, ignoring upload request\r\n");
 		return;
 	}
 
 	if (is_rate_limited(&_ratelimiterdata,xTaskGetTickCount())) {
-		LOGI("audio_features_upload -- rate limited, ignoring upload request");
+		LOGI("audio_features_upload -- rate limited, ignoring upload request\r\n");
 		return;
 	}
 
