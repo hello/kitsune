2489f7fef21b0d8450cbded3e4ab80aaf54a718a
Chris Johnson <plasticchris@users.noreply.github.com>
2016-12-09 10:51:00 -0800

    volume down for keywords, resume after (#710)
    * volume down for keywords, resume after
    
    * stop at zero
    
    * Revert "stop at zero"
    
    This reverts commit b70b4de447c8d94158a7d9a1a4dcbf7a76568798.
    
    * stability
    
    * use queues
    
    * wip
    
    * don't free an unlocked stream!
    
    * cleanup
    
    * tweak timings
    
    * Revert "wip"
    
    This reverts commit b6fee667c879db8c71ea5c221245c406e64d7d54.
    
    * prevent overflows, don't read from dead streams
    
    * concurrency is hard

--- kitsune/audiotask.c
+++ kitsune/audiotask.c
@@ -52,8 +52,9 @@
 
 #define MONO_BUF_LENGTH (AUDIO_FFT_SIZE) //256
 
-#define FLAG_SUCCESS (0x01)
-#define FLAG_STOP    (0x02)
+#define FLAG_SUCCESS     (0x01)
+#define FLAG_STOP        (0x02)
+#define FLAG_INTERRUPTED (0x03)
 
 /* static variables  */
 static xQueueHandle _playback_queue = NULL;
@@ -151,7 +152,7 @@ static uint8_t CheckForInterruptionDuringPlayback(void * unused) {
 			LOGI("Stopping playback\r\n");
 		}
 		if (m.command == eAudioPlaybackStart ) {
-			ret = FLAG_STOP;
+			ret = FLAG_INTERRUPTED;
 //			LOGI("Switching audio\r\n");
 		}
 	}
@@ -161,49 +162,53 @@ static uint8_t CheckForInterruptionDuringPlayback(void * unused) {
 extern xSemaphoreHandle i2c_smphr;
 
 typedef struct{
-	unsigned long current;
-	unsigned long target;
+	long current;
+	long target;
 	unsigned long ramp_up_ms;
 	unsigned long ramp_down_ms;
 	int32_t  duration;
 }ramp_ctx_t;
 
 int32_t set_volume(int v, unsigned int dly);
+int32_t reduce_volume( int v, unsigned int dly );
 
 static void _change_volume_task(hlo_future_t * result, void * ctx){
-	volatile ramp_ctx_t * v = (ramp_ctx_t*)ctx;
+	xQueueHandle volume_queue =  (xQueueHandle)ctx;
+	ramp_ctx_t v;
+	xQueueReceive( volume_queue,(void *) &v, portMAX_DELAY );
+
+	long last_set_vol = 0;
 	portTickType t0 = xTaskGetTickCount();
-	while( v->target || v->current ){
-		if ( (v->duration - (int32_t)(xTaskGetTickCount() - t0)) < 0 && v->duration > 0){
-			v->target = 0;
+	portTickType last_set = 0;
+	while( v.target != 0 || v.current != 0 ){
+		if ( (v.duration - (int32_t)(xTaskGetTickCount() - t0)) < 0 && v.duration > 0){
+			v.target = 0;
 		}
-		if(v->current > v->target){
-			vTaskDelay(v->ramp_down_ms);
-			v->current--;
-		}else if(v->current < v->target){
-			v->current++;
-			vTaskDelay(v->ramp_up_ms);
-		}else{
-			vTaskDelay(10);
-			continue;
+		if(v.current > v.target){
+			vTaskDelay(v.ramp_down_ms);
+			v.current--;
+		}else if(v.current < v.target){
+			v.current++;
+			vTaskDelay(v.ramp_up_ms);
 		}
-		//fallthrough if volume adjust is needed
-		if(v->current % 10 == 0){
-			LOGI("Setting volume %u at %u\n", v->current, xTaskGetTickCount());
+		if((v.target<0) || (v.current<0)) {
+			break;
 		}
 
-		if( xSemaphoreTakeRecursive(i2c_smphr, 10)) {
-			//set vol
-			vTaskDelay(5);
-			set_volume(v->current, 0);
-			vTaskDelay(5);
-			xSemaphoreGiveRecursive(i2c_smphr);
+		if( ( v.current != last_set_vol ) && ( xTaskGetTickCount() - last_set > 10 ) ) {
+			DISP("%u %u at %u\n", v.current, v.target, xTaskGetTickCount());
+			set_volume(v.current, 0);
+			last_set_vol = v.current;
+			last_set = xTaskGetTickCount();
+		} else {
+			vTaskDelay(1);
+		}
+		if(v.current == v.target && (v.current != 0) ) {
+			xQueueReceive( volume_queue,(void *) &v, portMAX_DELAY );
 		}
 	}
-	AudioTask_StopPlayback();
+	set_volume(v.current<0?0:v.current, 100);
 	hlo_future_write(result, NULL, 0, 0);
-
-
 }
 
 static uint8_t fadeout_sig(void * ctx) {
@@ -215,26 +220,30 @@ static uint8_t fadeout_sig(void * ctx) {
 	return 0;
 }
 
-extern volatile int last_set_volume;
+extern volatile int last_fadeout_volume;
 
 ////-------------------------------------------
 //playback sample app
-static void _playback_loop(AudioPlaybackDesc_t * desc, hlo_stream_signal sig_stop){
-	int ret;
+static int _playback_loop(AudioPlaybackDesc_t * desc, hlo_stream_signal sig_stop){
+	int main_ret=FLAG_STOP,ret=FLAG_STOP;
 	bool  vol_ramp = desc->fade_in_ms || desc->fade_out_ms || desc->to_fade_out_ms;
 	ramp_ctx_t vol;
 	hlo_future_t * vol_task;
+	xQueueHandle volume_queue = NULL;
 
 	hlo_stream_t * spkr = hlo_audio_open_mono(desc->rate,HLO_AUDIO_PLAYBACK);
 	hlo_stream_t * fs = desc->stream;
 
 	if(vol_ramp) {
+		volume_queue = xQueueCreate(1,sizeof(ramp_ctx_t));
 		int ramp_target = desc->volume;
 		if(ramp_target > 64){
 			ramp_target = 64;
 		}else if(ramp_target < 0){
 			ramp_target = 0;
 		}
+		set_volume(0, portMAX_DELAY);
+
 		vol = (ramp_ctx_t){
 			.current = 0,
 			.target = ramp_target,
@@ -242,8 +251,8 @@ static void _playback_loop(AudioPlaybackDesc_t * desc, hlo_stream_signal sig_sto
 			.ramp_down_ms = desc->fade_out_ms / (desc->volume + 1),
 			.duration =  desc->durationInSeconds * 1000,
 		};
-		set_volume(0, portMAX_DELAY);
-		vol_task = (hlo_future_t*)hlo_future_create_task_bg(_change_volume_task,(void*)&vol,1024);
+		xQueueSend(volume_queue, &vol, portMAX_DELAY);
+		vol_task = (hlo_future_t*)hlo_future_create_task_bg(_change_volume_task,(void*)volume_queue,2048);
 	} else {
 		set_volume(desc->volume, portMAX_DELAY);
 	}
@@ -251,23 +260,43 @@ static void _playback_loop(AudioPlaybackDesc_t * desc, hlo_stream_signal sig_sto
 	//playback
 	hlo_filter transfer_function = desc->p ? desc->p : hlo_filter_data_transfer;
 
-	ret = transfer_function(fs, spkr, desc->context, sig_stop);
+	main_ret = transfer_function(fs, spkr, desc->context, sig_stop);
 
-	if( vol_ramp && ret > 0 ) {
+	if( vol_ramp ) {
+		vTaskDelay(1000*RX_BUFFER_SIZE/(2*AUDIO_SAMPLE_RATE) - 200 ); //wait for buffer to almost drain...
 		//join async worker
 		vol.target = 0;
-		vol.current = last_set_volume; //handles fade out if the system volume has changed during the last playback
-		ret = transfer_function(fs, spkr, vol_task, fadeout_sig);
+		vol.current = last_fadeout_volume; //handles fade out if the system volume has changed during the last playback
+		xQueueSend(volume_queue, &vol, portMAX_DELAY);
+
+		if( main_ret > 0 ) {
+			ret = transfer_function(fs, spkr, vol_task, fadeout_sig);
+			if( ret != FLAG_STOP ) {
+				hlo_future_read_once(vol_task, NULL, 0);
+			}
+			hlo_future_destroy(vol_task);
+		} else {
+			hlo_future_read_once(vol_task, NULL, 0);
+		}
+		vQueueDelete(volume_queue);
 	}
 
-	hlo_stream_close(fs);
-	hlo_stream_close(spkr);
-	if(desc->onFinished){
-		desc->onFinished(desc->context);
+	if( main_ret != FLAG_INTERRUPTED ) {
+		hlo_stream_close(fs);
+		if(desc->onFinished){
+			desc->onFinished(desc->context);
+		}
 	}
-	DISP("Playback Task Finished %d\r\n", ret);
+	hlo_stream_close(spkr);
+	DISP("Playback Task Finished %d, %d\r\n", main_ret, ret);
+
+	return main_ret;
 }
+
 void AudioPlaybackTask(void * data) {
+	AudioMessage_t m_resume;
+	m_resume.command = eAudioPlaybackStop;
+
 	_playback_queue = xQueueCreate(INBOX_QUEUE_LENGTH,sizeof(AudioMessage_t));
 	assert(_playback_queue);
 
@@ -278,23 +307,47 @@ void AudioPlaybackTask(void * data) {
 	hlo_future_t * state_update_task = hlo_future_create_task_bg(_sense_state_task, NULL, 1024);
 	assert(state_update_task);
 
+	int intdepth = 0;
+
 	while(1){
 		AudioMessage_t  m;
+
 		if (xQueueReceive( _playback_queue,(void *) &m, portMAX_DELAY )) {
+			top:
+
 			switch (m.command) {
 
 				case eAudioPlaybackStart:
 				{
+					int r;
 					AudioPlaybackDesc_t * info = &m.message.playbackdesc;
 					/** prep  **/
 					_queue_audio_playback_state(PLAYING, info);
 					/** blocking loop to play the sound **/
-					_playback_loop(info, CheckForInterruptionDuringPlayback);
-					/** clean up **/
-					_queue_audio_playback_state(SILENT, info);
+					r = _playback_loop(info, CheckForInterruptionDuringPlayback);
 
-					if (m.message.playbackdesc.onFinished) {
-						m.message.playbackdesc.onFinished(m.message.playbackdesc.context);
+					if( r == FLAG_STOP) {
+						/** clean up **/
+						_queue_audio_playback_state(SILENT, info);
+					}
+
+					if( r == FLAG_INTERRUPTED ) {
+						if( intdepth == 0 ) {
+							DISP("interrupted %d\n\n\n", intdepth);
+							m_resume = m;
+							intdepth++;
+						} else {
+							hlo_stream_close(info->stream);
+							if(info->onFinished){
+								info->onFinished(info->context);
+							}
+						}
+					} else if( m_resume.command != eAudioPlaybackStop) {
+						DISP("resumed %d\n\n\n", intdepth);
+						m = m_resume;
+						m_resume.command = eAudioPlaybackStop;
+						intdepth--;
+						goto top;
 					}
 				}   break;
 				default:
