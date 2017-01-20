2a7db95662c57bb075524de64cd31b1f032ade70
Chris <chris@sayhello.com]>
2017-01-20 09:06:55 -0800

    Benjamin Joseph [6:43 PM] model_nov07_lstm_med_adam_okay_sense_stop_snooze_tiny_end0_plus_1115_ep065
    [6:43]
    Test in sound box before shipping
    
    [6:44]
    It should pass 18/30

--- kitsune/tensor/keyword_net.c
+++ kitsune/tensor/keyword_net.c
@@ -15,7 +15,7 @@
 
 #include "hlo_http.h"
 
-#define NEURAL_NET_MODEL "model_aug30_lstm_med_stateful_okay_sense_stop_snooze_tiny_end0_plus_1115_ep050.c"
+#define NEURAL_NET_MODEL "model_nov07_lstm_med_adam_okay_sense_stop_snooze_tiny_end0_plus_1115_ep065.c"
 #include NEURAL_NET_MODEL
 const static char * k_net_id = NEURAL_NET_MODEL;
 
